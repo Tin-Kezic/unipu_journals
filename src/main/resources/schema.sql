d615170962bd4703c0b39a5bd8844ab1971fe139
Dorijan Rijavec <rijavecdorijan@gmail.com>
2025-08-01 21:19:00 +0200

    Baza 1.8 21:00

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -78,9 +78,9 @@ CREATE TABLE manuscript (
     publication_section_id INT NOT NULL,
     file_url TEXT NOT NULL,
     submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    round INT NOT NULL DEFAULT 0,
+    round INT NOT NULL DEFAULT 1,
     views INT NOT NULL DEFAULT 0,
-    downloads INT NOT NULL DEFAULT 1,
+    downloads INT NOT NULL DEFAULT 0,
     FOREIGN KEY (author_id) REFERENCES account(id) ON DELETE NO ACTION,
     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE NO ACTION,
     FOREIGN KEY (publication_section_id) REFERENCES publication_section(id) ON DELETE NO ACTION
