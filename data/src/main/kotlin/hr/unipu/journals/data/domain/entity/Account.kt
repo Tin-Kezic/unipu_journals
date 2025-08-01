7c6a7df576df9771079e42733f02508c71729be7
Tin Kežić <kezic.tin@gmail.com>
2025-08-01 02:23:39 +0200

    completed account table data class

---
+++ data/src/main/kotlin/hr/unipu/journals/data/domain/entity/Account.kt
@@ -0,0 +1,22 @@
+package hr.unipu.journals.data.domain.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("account")
+data class Account(
+    @Id
+    val id: Long = 0,
+    val name: String,
+    val surname: String,
+    val title: String,
+    val email: String, // converts to Email valueObject in domain
+    val password: String,
+    val affiliation: String,
+    val jobType: String,
+    val country: String,
+    val city: String,
+    val address: String,
+    val zipCode: String,
+)
