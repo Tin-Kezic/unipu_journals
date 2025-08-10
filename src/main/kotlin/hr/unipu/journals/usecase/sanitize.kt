3f7815c2fab47b3188b81d5ec0c305164f6dd416
Tin Kežić <kezic.tin@gmail.com>
2025-08-10 19:18:02 +0200

    updated sanitization to take SafeList as a parameter with .none() being the default

--- src/main/kotlin/hr/unipu/journals/usecase/sanitize.kt
+++ src/main/kotlin/hr/unipu/journals/usecase/sanitize.kt
@@ -3,4 +3,4 @@ package hr.unipu.journals.usecase
 import org.jsoup.Jsoup
 import org.jsoup.safety.Safelist
 
-fun sanitize(input: String?) = Jsoup.clean(input ?: "", Safelist.none())
+fun sanitize(input: String?, safelist: Safelist = Safelist.none()) = Jsoup.clean(input ?: "", safelist)
