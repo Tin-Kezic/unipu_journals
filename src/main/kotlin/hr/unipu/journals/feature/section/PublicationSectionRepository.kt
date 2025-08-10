7da0562f80f763866c34108dcf23f8a53a39afa4
Tin Kežić <kezic.tin@gmail.com>
2025-08-10 12:43:26 +0200

    updated section feature

--- src/main/kotlin/hr/unipu/journals/feature/section/PublicationSectionRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section/PublicationSectionRepository.kt
@@ -1,5 +1,38 @@
 package hr.unipu.journals.feature.section
 
+import hr.unipu.journals.feature.publication.Publication
+import org.springframework.data.jdbc.repository.query.Modifying
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
+import org.springframework.data.repository.query.Param
 
-interface PublicationSectionRepository: Repository<Section, Int>
+private const val PUBLICATION_SECTION = "publication_section"
+interface PublicationSectionRepository: Repository<Section, Int> {
+
+    // view
+    @Query("SELECT * FROM $PUBLICATION_SECTION")
+    fun all(): List<Publication>
+
+    @Query("SELECT * FROM $PUBLICATION_SECTION WHERE id = :id")
+    fun findById(@Param("id") id: Int): Publication
+
+    // REST
+    @Modifying
+    @Query("INSERT INTO $PUBLICATION_SECTION (title) VALUES (:title)")
+    fun insert(@Param("title") title: String)
+
+    @Modifying
+    @Query("UPDATE $PUBLICATION_SECTION SET title = :title WHERE id = :id")
+    fun update(@Param("id") id: Int, @Param("title") title: String)
+
+    @Query("SELECT EXISTS (SELECT 1 FROM $PUBLICATION_SECTION WHERE id = :id)")
+    fun existsById(@Param("id") id: Int): Boolean
+
+    @Modifying
+    @Query("UPDATE $PUBLICATION_SECTION SET is_hidden = TRUE where id = :id")
+    fun hide(@Param("id") id: Int)
+
+    @Modifying
+    @Query("DELETE FROM $PUBLICATION_SECTION WHERE id = :id")
+    fun delete(@Param("id") id: Int)
+}
--- src/main/kotlin/hr/unipu/journals/view/home/SectionPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/home/SectionPageController.kt
@@ -1,4 +1,24 @@
 package hr.unipu.journals.controller.view.home
 
-class SectionPageController {
+import hr.unipu.journals.feature.publication.PublicationRepository
+import hr.unipu.journals.feature.section.PublicationSectionRepository
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/publication/{id}")
+class SectionPageController(
+    private val publicationRepository: PublicationRepository,
+    private val publicationSectionRepository: PublicationSectionRepository
+) {
+
+    @GetMapping("/")
+    fun findAll(model: Model): String {
+        model["publications"] = publicationRepository.all()
+        model["sections"] = publicationSectionRepository.all()
+        return "home/section-page"
+    }
 }
