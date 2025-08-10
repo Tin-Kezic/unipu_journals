9100e8be02941893fa29cf1043b50d1bb130a1f3
Tin Kežić <kezic.tin@gmail.com>
2025-08-10 19:18:26 +0200

    changes

--- src/main/kotlin/hr/unipu/journals/feature/publication/PublicationController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/publication/PublicationController.kt
@@ -1,7 +1,6 @@
 package hr.unipu.journals.feature.publication
 
 import hr.unipu.journals.usecase.sanitize
-import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,27 +13,24 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/publication")
 class PublicationController(private val repository: PublicationRepository) {
 
-    @PostMapping("/insert")
+    @PostMapping("/insert-publication")
     fun insert(@ModelAttribute title: String): ResponseEntity<String> {
-        return try {
+        return if(title.isNotEmpty()) {
             repository.insert(sanitize(title))
             ResponseEntity.ok().body("account successfully added")
-        } catch (_: IllegalArgumentException) {
-            ResponseEntity.badRequest().body("Invalid account data. title must be non-null")
-        } catch (_: OptimisticLockingFailureException) {
-            ResponseEntity.internalServerError().body("internal server error of type OptimisticLockingFailureException")
-        }
+        } else ResponseEntity.badRequest().body("title must not be empty")
     }
-    @PutMapping("/update")
+    @PutMapping("/update-publication-title")
     fun update(@ModelAttribute id: Int, @ModelAttribute title: String): ResponseEntity<String> {
-        if(!repository.existsById(id)) return ResponseEntity.badRequest().body("publication with id: $id does not exist")
-        repository.update(id, title)
-        return ResponseEntity.ok().body("title successfully updated")
+        return if(repository.existsById(id)) {
+            repository.updateTitle(id, title)
+            ResponseEntity.ok().body("title successfully updated")
+        } else ResponseEntity.badRequest().body("publication with id: $id does not exist")
     }
-    @PutMapping("/hide/{id}")
-    fun hidePublication(@PathVariable id: Int): ResponseEntity<String> {
-        return if (repository.existsById(id)) {
-            repository.hide(id)
+    @PutMapping("/hide/{publicationId}")
+    fun hidePublication(@PathVariable publicationId: Int): ResponseEntity<String> {
+        return if (repository.existsById(publicationId)) {
+            repository.hide(publicationId)
             ResponseEntity.ok().body("publication successfully hidden")
         } else ResponseEntity.badRequest().body("id does not exist")
     }
--- src/main/kotlin/hr/unipu/journals/feature/section/SectionController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section/SectionController.kt
@@ -1,6 +1,7 @@
 package hr.unipu.journals.feature.section
 
 import hr.unipu.journals.usecase.sanitize
+import org.jsoup.safety.Safelist
 import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -13,14 +14,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/publication/")
 class SectionController(private val repository: SectionRepository) {
-
     @PostMapping("{publicationId}/insert")
     fun insert(
         @PathVariable publicationId: Int,
-        @ModelAttribute title: String
+        @ModelAttribute title: String,
+        @ModelAttribute description: String
     ): ResponseEntity<String> {
         return try {
-            repository.insert(sanitize(title))
+            repository.insert(
+                title = sanitize(title),
+                description = sanitize(description),
+                publicationId = publicationId,
+            )
             ResponseEntity.ok().body("account successfully added")
         } catch (_: IllegalArgumentException) {
             ResponseEntity.badRequest().body("Invalid account data. title must be non-null")
@@ -28,22 +33,22 @@ class SectionController(private val repository: SectionRepository) {
             ResponseEntity.internalServerError().body("internal server error of type OptimisticLockingFailureException")
         }
     }
-    @PutMapping("{publicationId}/update/{sectionId}")
+    @PutMapping("{publicationId}/updateTitle")
     fun update(
         @PathVariable publicationId: Int,
         @PathVariable sectionId: Int,
         @ModelAttribute title: String
     ): ResponseEntity<String> {
-        if(!repository.existsById(sectionId)) return ResponseEntity.badRequest().body("section with id: $sectionId does not exist")
-        repository.update(sectionId, title)
-        return ResponseEntity.ok().body("title successfully updated")
+        return if (repository.existsById(sectionId)) {
+            repository.updateTitle(sectionId, title)
+            ResponseEntity.ok().body("title successfully updated")
+        } else ResponseEntity.badRequest().body("section with id: $sectionId does not exist")
     }
     @PutMapping("{publicationId}/hide/{section_id}")
     fun hidePublication(
         @PathVariable publicationId: Int,
         @PathVariable sectionId: Int,
     ): ResponseEntity<String> {
-
         return if (repository.existsById(sectionId)) {
             repository.hide(sectionId)
             ResponseEntity.ok().body("publication successfully hidden")
