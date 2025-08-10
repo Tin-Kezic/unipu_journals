ac0da1b552e215bd20ab0355492936a5c5d97589
Tin Kežić <kezic.tin@gmail.com>
2025-08-10 12:43:08 +0200

    completed publication RestController and Controller for views

--- src/main/kotlin/hr/unipu/journals/feature/publication/PublicationRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/publication/PublicationRepository.kt
@@ -5,30 +5,33 @@ import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
+private const val PUBLICATION = "publication"
 interface PublicationRepository: Repository<Publication, Int> {
 
+    // view
+    @Query("SELECT * FROM $PUBLICATION")
+    fun all(): List<Publication>
+
+    @Query("SELECT * FROM $PUBLICATION WHERE id = :id")
+    fun findById(@Param("id") id: Int): Publication
+
+    // REST
     @Modifying
-    @Query("INSERT INTO publication (title) VALUES (:title)")
+    @Query("INSERT INTO $PUBLICATION (title) VALUES (:title)")
     fun insert(@Param("title") title: String)
 
     @Modifying
-    @Query("UPDATE publication SET title = :title WHERE id = :id")
+    @Query("UPDATE $PUBLICATION SET title = :title WHERE id = :id")
     fun update(@Param("id") id: Int, @Param("title") title: String)
 
-    @Query("SELECT * FROM publication")
-    fun all(): List<Publication>
-
-    @Query("SELECT * FROM publication WHERE id = :id")
-    fun findById(@Param("id") id: Int): Publication
-
-    @Query("SELECT EXISTS (SELECT 1 FROM publication WHERE id = :id)")
+    @Query("SELECT EXISTS (SELECT 1 FROM $PUBLICATION WHERE id = :id)")
     fun existsById(@Param("id") id: Int): Boolean
 
     @Modifying
-    @Query("UPDATE publication SET is_hidden = TRUE where id = :id")
+    @Query("UPDATE $PUBLICATION SET is_hidden = TRUE where id = :id")
     fun hide(@Param("id") id: Int)
 
     @Modifying
-    @Query("DELETE FROM publication WHERE id = :id")
+    @Query("DELETE FROM $PUBLICATION WHERE id = :id")
     fun delete(@Param("id") id: Int)
 }
