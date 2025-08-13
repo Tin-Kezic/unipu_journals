4ae8a27cdfeb6cfa0c7754b601a371a6279b1f5f
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 01:29:04 +0200

    added RoleContextService.kt, incomplete.

---
+++ src/main/kotlin/hr/unipu/journals/security/RoleContextService.kt
@@ -0,0 +1,29 @@
+package hr.unipu.journals.security
+
+import hr.unipu.journals.feature.account.AccountRepository
+import hr.unipu.journals.feature.account_role_on_manuscript.AccountRoleOnManuscriptRepository
+import hr.unipu.journals.feature.eic_on_publication.EicOnPublicationRepository
+import hr.unipu.journals.feature.manuscript_review.ManuscriptReviewRepository
+import hr.unipu.journals.feature.section_editor_on_section.SectionEditorOnSectionRepository
+import org.springframework.stereotype.Service
+
+@Service
+class RoleContextService(
+    private val accountRepository: AccountRepository,
+    private val eicOnPublicationRepository: EicOnPublicationRepository,
+    private val sectionEditorOnSectionRepository: SectionEditorOnSectionRepository,
+    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
+    private val manuscriptReviewRepository: ManuscriptReviewRepository
+) {
+    fun isAdmin(accountId: Int) = accountRepository.isAdmin(accountId)
+    fun isEiCOnPublication(publicationId: Int, eicId: Int): Boolean {
+        return eicOnPublicationRepository.isEicOnPublication(publicationId, eicId)
+    }
+    fun isEicOnManuscript {}
+    fun isSectionEditorOnSection {}
+    fun isEditorOnManuscript {}
+    fun isReviewerOnManuscript {}
+    fun isCorrespondingAuthorOnManuscript {}
+    fun isAuthorOnManuscript {}
+
+}
