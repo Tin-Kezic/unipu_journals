bd77260acc640b2ef5fb494710d21f98a98e7df7
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 00:19:03 +0200

    created SecurityUser.kt

---
+++ src/main/kotlin/hr/unipu/journals/security/SecurityUser.kt
@@ -0,0 +1,14 @@
+package hr.unipu.journals.security
+
+import hr.unipu.journals.feature.account.Account
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class SecurityUser(
+    private val account: Account,
+    private val authority: Collection<GrantedAuthority>
+): UserDetails {
+    override fun getAuthorities() = authority
+    override fun getPassword() = account.password
+    override fun getUsername() = account.email
+}
