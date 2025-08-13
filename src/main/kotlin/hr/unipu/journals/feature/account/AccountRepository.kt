8e2ec687d20a54ef59d712ce92c6fc3ffc549992
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 01:28:27 +0200

    added queries to AccountRepository for byEmail, emailExists and isAdmin

--- src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
@@ -4,4 +4,12 @@ import org.springframework.data.repository.Repository
 
 interface AccountRepository: Repository<Account, Int> {
 
+    @Query("SELECT * FROM $ACCOUNT WHERE $EMAIL = :$EMAIL")
+    fun byEmail(@Param(EMAIL) email: String)
+
+    @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT WHERE $EMAIL = :$EMAIL")
+    fun emailExists(@Param(EMAIL) email: String): Boolean
+
+    @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT WHERE $IS_ADMIN = TRUE AND $ID = :$ID")
+    fun isAdmin(@Param(ID) id: Int): Boolean
 }
