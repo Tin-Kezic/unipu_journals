e7f1a76e3610744846789a31a3f86a163579d750
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 16:44:05 +0200

    added const values to SectionEditorOnSectionRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionRepository.kt
@@ -2,4 +2,8 @@ package hr.unipu.journals.feature.section_editor_on_section
 
 import org.springframework.data.repository.Repository
 
+private const val SECTION_EDITOR_ON_SECTION = "section_editor_on_section"
+private const val ID = "id"
+private const val PUBLICATION_SECTION_ID = "publication_section_id"
+private const val SECTION_EDITOR_ID = "section_editor_id"
 interface SectionEditorOnSectionRepository: Repository<SectionEditorOnSection, Int>
