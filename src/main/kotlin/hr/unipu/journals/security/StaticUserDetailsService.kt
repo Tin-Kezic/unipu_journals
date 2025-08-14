e1a0fc671fdc464339f6d59bc2802bdc60c49c9d
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 22:21:57 +0200

    renamed CustomUserDetailsService.kt to StaticUserDetailsService.kt and simplified

--- src/main/kotlin/hr/unipu/journals/security/StaticUserDetailsService.kt
+++ src/main/kotlin/hr/unipu/journals/security/StaticUserDetailsService.kt
@@ -7,16 +7,13 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 
-class CustomUserDetailsService(
+class StaticUserDetailsService(
     private val accountRepository: AccountRepository,
     private val roleContextService: RoleContextService
 ): UserDetailsService {
     override fun loadUserByUsername(email: String): UserDetails {
         if(!accountRepository.emailExists(email)) throw UsernameNotFoundException("$email not found")
-
         val account = accountRepository.byEmail(email)
-        val authority = mutableListOf<GrantedAuthority>()
-
-        if(account.isAdmin) authority.add(SimpleGrantedAuthority(Role.ADMIN.name))
+        return SecurityUser(account, if(account.isAdmin) listOf(SimpleGrantedAuthority(ADMIN)) else emptyList())
     }
 }
