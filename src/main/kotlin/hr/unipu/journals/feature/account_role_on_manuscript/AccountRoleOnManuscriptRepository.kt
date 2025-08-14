ff977ae27695d303130a33e7ea1a00b239dda990
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 17:19:49 +0200

    completed AccountRoleOnManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -11,7 +11,35 @@ private const val MANUSCRIPT_ID = "manuscript_id"
 private const val ACCOUNT_ID = "account_id"
 private const val ACCOUNT_ROLE = "account_role"
 interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript, Int> {
-    @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID")
+    @Query("""
+        SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT
+        WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID
+        AND $ACCOUNT_ROLE = 'EIC'
+    """)
     fun isEicOnManuscript(@Param(ACCOUNT_ID) eicId: Int, @Param(MANUSCRIPT_ID) manuscriptId: Int)
+    @Query("""
+        SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT
+        WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID
+        AND $ACCOUNT_ROLE = 'EDITOR'
+    """)
+    fun isEditorOnManuscript(@Param(ACCOUNT_ID) editorId: Int, @Param(MANUSCRIPT_ID) manuscriptId: Int)
+    @Query("""
+        SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT
+        WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID
+        AND $ACCOUNT_ROLE = 'REVIEWER'
+    """)
+    fun isReviewerOnManuscript(@Param(ACCOUNT_ID) reviewerId: Int, @Param(MANUSCRIPT_ID) manuscriptId: Int)
+    @Query("""
+        SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT
+        WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID
+        AND $ACCOUNT_ROLE = 'CORRESPONDING_AUTHOR'
+    """)
+    fun isCorrespondingAuthorOnManuscript(@Param(ACCOUNT_ID) correspondingAuthorId: Int, @Param(MANUSCRIPT_ID) manuscriptId: Int)
+    @Query("""
+        SELECT EXISTS (SELECT 1 FROM $ACCOUNT_ROLE_ON_MANUSCRIPT
+        WHERE $MANUSCRIPT_ID = :$MANUSCRIPT_ID AND $ACCOUNT_ID = :$ACCOUNT_ID
+        AND $ACCOUNT_ROLE = 'AUTHOR'
+    """)
+    fun isAuthorOnManuscript(@Param(ACCOUNT_ID) authorId: Int, @Param(MANUSCRIPT_ID) manuscriptId: Int)
 }
 
