da7dc1e282388e595437d306490c1798e34cd4e9
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 16:37:16 +0200

    added const values to AccountRoleOnManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -1,6 +1,13 @@
 package hr.unipu.journals.feature.account_role_on_manuscript
 
 import org.springframework.data.repository.Repository
+import org.springframework.data.repository.query.Param
+
+private const val ACCOUNT_ROLE_ON_MANUSCRIPT = "account_role_on_manuscript"
+private const val ID = "id"
+private const val MANUSCRIPT_ID = "manuscript_id"
+private const val ACCOUNT_ID = "account_id"
+private const val ACCOUNT_ROLE = "account_role"
 
 interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript, Int>
 
