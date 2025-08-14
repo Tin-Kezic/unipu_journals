99df4e7511631c698630082d2ca29d40ddaaad95
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 22:32:00 +0200

    shortened Role.kt names

--- src/main/kotlin/hr/unipu/journals/security/Role.kt
+++ src/main/kotlin/hr/unipu/journals/security/Role.kt
@@ -1,10 +1,10 @@
 package hr.unipu.journals.security
 
-const val ROLE_ROOT = "ROOT"
-const val ROLE_ADMIN = "ADMIN"
-const val ROLE_EIC = "EIC"
-const val ROLE_SECTION_EDITOR = "SECTION_EDITOR"
-const val ROLE_EDITOR = "EDITOR"
-const val ROLE_REVIEWER = "REVIEWER"
-const val ROLE_CORRESPONDING_AUTHOR = "CORRESPONDING_AUTHOR"
-const val ROLE_AUTHOR = "AUTHOR"
+const val ROOT = "ROOT"
+const val ADMIN = "ADMIN"
+const val EIC = "EIC"
+const val SECTION_EDITOR = "SECTION_EDITOR"
+const val EDITOR = "EDITOR"
+const val REVIEWER = "REVIEWER"
+const val CORRESPONDING_AUTHOR = "CORRESPONDING_AUTHOR"
+const val AUTHOR = "AUTHOR"
