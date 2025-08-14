aedc6ece8313fe3ff969bcb0c60110dd129a1328
Tin Kežić <kezic.tin@gmail.com>
2025-08-14 20:49:23 +0200

    placed enum with const values

--- src/main/kotlin/hr/unipu/journals/security/Role.kt
+++ src/main/kotlin/hr/unipu/journals/security/Role.kt
@@ -1,12 +1,10 @@
 package hr.unipu.journals.security
 
-enum class Role {
-    ROOT,
-    ADMIN,
-    EIC,
-    SECTION_EDITOR,
-    EDITOR,
-    REVIEWER,
-    CORRESPONDING_AUTHOR,
-    AUTHOR,
-}
+const val ROLE_ROOT = "ROOT"
+const val ROLE_ADMIN = "ADMIN"
+const val ROLE_EIC = "EIC"
+const val ROLE_SECTION_EDITOR = "SECTION_EDITOR"
+const val ROLE_EDITOR = "EDITOR"
+const val ROLE_REVIEWER = "REVIEWER"
+const val ROLE_CORRESPONDING_AUTHOR = "CORRESPONDING_AUTHOR"
+const val ROLE_AUTHOR = "AUTHOR"
