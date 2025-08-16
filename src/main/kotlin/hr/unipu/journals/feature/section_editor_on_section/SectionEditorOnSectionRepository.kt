880da0b367982cd42516b60eff7cf84c602df9b0
Tin Kežić <kezic.tin@gmail.com>
2025-08-16 19:03:01 +0200

    fixed order of const values in SectionEditorOnSectionRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionRepository.kt
@@ -4,8 +4,8 @@ import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
-private const val SECTION_EDITOR_ON_SECTION = "section_editor_on_section"
 private const val ID = "id"
+private const val SECTION_EDITOR_ON_SECTION = "section_editor_on_section"
 private const val PUBLICATION_SECTION_ID = "publication_section_id"
 private const val SECTION_EDITOR_ID = "section_editor_id"
 interface SectionEditorOnSectionRepository: Repository<SectionEditorOnSection, Int> {
