c333c06cae87b2fea8ae7eb513707b8542672492
Tin Kežić <kezic.tin@gmail.com>
2025-08-16 18:38:24 +0200

    added const values for CategoryRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/category/CategoryRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/category/CategoryRepository.kt
@@ -2,6 +2,7 @@ package hr.unipu.journals.feature.category
 
 import org.springframework.data.repository.Repository
 
+private const val CATEGORY = "category"
 private const val ID = "id"
 private const val NAME = "name"
 interface CategoryRepository: Repository<Category, Int>
