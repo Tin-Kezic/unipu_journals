e9a464c9f70ccae222e315d0eccbc5c01e79cae0
Tin Kežić <kezic.tin@gmail.com>
2025-08-16 18:43:51 +0200

    added const values to ManuscriptReviewRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript_review/ManuscriptReviewRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript_review/ManuscriptReviewRepository.kt
@@ -1,4 +1,15 @@
 package hr.unipu.journals.feature.manuscript_review
 
+private const val MANUSCRIPT_REVIEW = "manuscript_review"
+private const val ID = "id"
+private const val MANUSCRIPT_ID = "manuscript_id"
+private const val REVIEW_ID = "review_id"
+private const val ROUND = "round"
+private const val REVIEWER_COMMENT = "reviewer_comment"
+private const val REVIEWER_COMMENT_FILE_URL = "reviewer_comment_file_url"
+private const val AUTHOR_RESPONSE = "author_response"
+private const val AUTHOR_RESPONSE_FILE_URL = "author_response_file_url"
+private const val REVIEW_DATE = "review_date"
+private const val AUTHOR_RESPONSE_DATE = "author_response_date"
 interface ManuscriptReviewRepository {
 }
