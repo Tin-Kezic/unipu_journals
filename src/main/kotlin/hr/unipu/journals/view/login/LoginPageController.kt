cf9b9e6590d0a9c95b644f6c39b6ad253c256527
Tin Kežić <kezic.tin@gmail.com>
2025-08-16 12:32:41 +0200

    added login and register controller

--- src/main/kotlin/hr/unipu/journals/view/login/LoginPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/login/LoginPageController.kt
@@ -1,4 +1,10 @@
 package hr.unipu.journals.view.login
 
-class LoginPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class LoginPageController() {
+    @GetMapping("/login")
+    fun all() = "/login/login-page"
 }
