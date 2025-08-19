7ee786f2259cf09afb4b168812f9475dc4988587
Tin Kežić <kezic.tin@gmail.com>
2025-08-20 01:38:50 +0200

    added ManuscriptState const values to ManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptRepository.kt
@@ -15,6 +15,12 @@ private const val SUBMISSION_DATE = "submission_date"
 private const val PUBLICATION_DATE = "publication_date"
 private const val VIEWS = "views"
 private const val DOWNLOADS = "downloads"
+
+// manuscript-state
+private const val PUBLISHED ="'PUBLISHED'"
+private const val ARCHIVED = "'ARCHIVED'"
+private const val HIDDEN = "'HIDDEN'"
+
 interface ManuscriptRepository: Repository<Manuscript, Int> {
     @Query("SELECT * FROM $MANUSCRIPT WHERE $SECTION_ID = :$SECTION_ID")
     fun allBySectionId(@Param(SECTION_ID) sectionId: Int): List<Manuscript>
