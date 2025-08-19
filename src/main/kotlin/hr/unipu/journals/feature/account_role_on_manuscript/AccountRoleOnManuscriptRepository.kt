d1cc214c5bcb6502f9775aba4e4cce7007012759
Tin Kežić <kezic.tin@gmail.com>
2025-08-20 01:22:03 +0200

    moved ID const

--- src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -5,8 +5,8 @@ import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
-private const val ACCOUNT_ROLE_ON_MANUSCRIPT = "account_role_on_manuscript"
 private const val ID = "id"
+private const val ACCOUNT_ROLE_ON_MANUSCRIPT = "account_role_on_manuscript"
 private const val MANUSCRIPT_ID = "manuscript_id"
 private const val ACCOUNT_ID = "account_id"
 private const val ACCOUNT_ROLE = "account_role"
