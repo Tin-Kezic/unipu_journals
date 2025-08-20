ff98bf504985d94ce62575560f6cde79c5da7adb
Tin Kežić <kezic.tin@gmail.com>
2025-08-20 20:53:20 +0200

    in AccountRepository.kt removed emailExists and instead made byEmail return Account? which can then be cached with an elvis operator and throw and exception more efficiently with one query than 2

--- src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
@@ -22,8 +22,5 @@ private const val IS_ADMIN = "is_admin"
 interface AccountRepository: Repository<Account, Int> {
 
     @Query("SELECT * FROM $ACCOUNT WHERE $EMAIL = :$EMAIL")
-    fun byEmail(@Param(EMAIL) email: String): Account
-
-    @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT WHERE $EMAIL = :$EMAIL")
-    fun emailExists(@Param(EMAIL) email: String): Boolean
+    fun byEmail(@Param(EMAIL) email: String): Account?
 }
