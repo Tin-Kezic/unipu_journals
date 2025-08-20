ce60d50ea0601885b4bde2c50ab148b3c4f7285b
Tin Kežić <kezic.tin@gmail.com>
2025-08-20 20:56:23 +0200

    added basic PendingReviewPageController

--- src/main/kotlin/hr/unipu/journals/view/review/PendingReviewPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/review/PendingReviewPageController.kt
@@ -1,4 +1,12 @@
 package hr.unipu.journals.view.review
 
-class PendingReviewPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/review")
+class PendingReviewPageController() {
+    @GetMapping("/pending")
+    fun page() = "review/pending-review-page"
 }
