8443548f2612f73faeda5734e1a5b5471217534c
Tin Kežić <kezic.tin@gmail.com>
2025-08-21 11:51:21 +0200

    implemented basic ReviewPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/review/ReviewPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/review/ReviewPageController.kt
@@ -1,4 +1,11 @@
 package hr.unipu.journals.view.review
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
-class ReviewPageController {
+@Controller
+@RequestMapping("/review")
+class ReviewPageController() {
+    @GetMapping("/{manuscriptId}")
+    fun page() = "review/review-page"
 }
