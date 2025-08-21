ddb94138258219d516e50c43d83fb5d5152c6646
Tin Kežić <kezic.tin@gmail.com>
2025-08-21 21:09:22 +0200

    implemented placeholder for ArchiveManuscriptPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/archive/ArchiveManuscriptPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/archive/ArchiveManuscriptPageController.kt
@@ -1,4 +1,13 @@
 package hr.unipu.journals.view.archive
 
-class ArchiveManuscriptPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/archive/publication")
+class ArchiveManuscriptPageController() {
+    @GetMapping("/{publicationId}/section/{sectionId}/")
+    fun page(@PathVariable publicationId: Int, @PathVariable sectionId: Int) = "placeholder.html"
 }
