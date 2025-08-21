70343115f58a51eb7c278342a1560e88fbe6a246
Tin Kežić <kezic.tin@gmail.com>
2025-08-21 21:09:55 +0200

    implemented placeholder for HiddenManuscriptPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/hidden/HiddenManuscriptPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/hidden/HiddenManuscriptPageController.kt
@@ -1,4 +1,13 @@
 package hr.unipu.journals.view.hidden
 
-class HiddenManuscriptPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/hidden/publication")
+class HiddenManuscriptPageController() {
+    @GetMapping("/{publicationId}/section/{sectionId}/")
+    fun page(@PathVariable publicationId: Int, @PathVariable sectionId: Int) = "placeholder.html"
 }
