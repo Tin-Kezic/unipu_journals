1db75b6b9a3b630336fa84b3c6dd21516288ef10
Tin Kežić <kezic.tin@gmail.com>
2025-08-21 21:09:36 +0200

    implemented placeholder for HiddenPublicationPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/hidden/HiddenPublicationPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/hidden/HiddenPublicationPageController.kt
@@ -1,4 +1,10 @@
 package hr.unipu.journals.view.hidden
 
-class HiddenPublicationPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HiddenPublicationPageController() {
+    @GetMapping("/hidden")
+    fun page() = "placeholder.html"
 }
