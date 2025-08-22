9f4bff5b54a88ff4b61b7c7052f82750abcbe50a
Tin Kežić <kezic.tin@gmail.com>
2025-08-22 15:59:06 +0200

    added AdminRepository import to SecurityConfig.kt

--- src/main/kotlin/hr/unipu/journals/security/SecurityConfig.kt
+++ src/main/kotlin/hr/unipu/journals/security/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package hr.unipu.journals.security
 
 import hr.unipu.journals.feature.account.AccountRepository
+import hr.unipu.journals.feature.admin.AdminRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
