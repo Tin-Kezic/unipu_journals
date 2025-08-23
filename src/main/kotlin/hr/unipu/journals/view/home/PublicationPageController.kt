0d719d331b216e67406c2669796b2b2d0dc9fd52
Tin Kežić <kezic.tin@gmail.com>
2025-08-23 15:02:52 +0200

    added authorizationService to PublicationPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/home/PublicationPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/home/PublicationPageController.kt
@@ -1,13 +1,17 @@
 package hr.unipu.journals.view.home
 
 import hr.unipu.journals.feature.publication.PublicationRepository
+import hr.unipu.journals.security.AuthorizationService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PublicationPageController(private val publicationRepository: PublicationRepository) {
+class PublicationPageController(
+    private val publicationRepository: PublicationRepository,
+    private val authorizationService: AuthorizationService
+) {
     @GetMapping("/")
     fun page(model: Model): String {
         model["publications"] = publicationRepository.allPublished()
