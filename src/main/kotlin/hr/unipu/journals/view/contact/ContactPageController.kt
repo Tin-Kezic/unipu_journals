af7e0be6c92fd5d8f4cfd9512cf2e5b10f7f5e58
Tin Kežić <kezic.tin@gmail.com>
2025-08-24 03:07:13 +0200

    made ContactPageController.kt redirect to placeholder.html

--- src/main/kotlin/hr/unipu/journals/view/contact/ContactPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/contact/ContactPageController.kt
@@ -1,4 +1,11 @@
 package hr.unipu.journals.view.contact
 
-class ContactPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+
+@Controller
+class ContactPageController() {
+    @GetMapping("/contact")
+    fun page() = "placeholder.html"
 }
