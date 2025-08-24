c7f4500ad1488856a9be37f953bd1d78d59a7c01
Tin Kežić <kezic.tin@gmail.com>
2025-08-24 22:28:06 +0200

    removed Admin.kt

--- src/main/kotlin/hr/unipu/journals/feature/admin/Admin.kt
+++
@@ -1,9 +0,0 @@
-package hr.unipu.journals.feature.admin
-
-import org.springframework.data.relational.core.mapping.Table
-
-@Table("admin")
-data class Admin(
-    val id: Int,
-    val email: String
-)
