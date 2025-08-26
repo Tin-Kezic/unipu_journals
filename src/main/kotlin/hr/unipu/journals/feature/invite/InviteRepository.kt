55b0221cdc29b56181784ff1ca25c70701b3703d
Tin Kežić <kezic.tin@gmail.com>
2025-08-26 19:57:12 +0200

    added const values and allAdmin method to InviteRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/invite/InviteRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/invite/InviteRepository.kt
@@ -1,4 +1,24 @@
 package hr.unipu.journals.feature.invite
 
-class InviteRepository {
+import hr.unipu.journals.feature.account.Account
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.Repository
+
+private const val INVITE = "invite"
+private const val ID = "id"
+private const val EMAIL = "email"
+private const val TARGET = "target"
+
+// invitation-target
+private const val ADMIN = "'ADMIN'"
+private const val EIC_ON_PUBLICATION = "'EIC_ON_PUBLICATION'"
+private const val EIC_ON_MANUSCRIPT = "'EIC_ON_MANUSCRIPT'"
+private const val SECTION_EDITOR_ON_SECTION = "'SECTION_EDITOR_ON_SECTION'"
+private const val EDITOR_ON_MANUSCRIPT = "'EDITOR_ON_MANUSCRIPT'"
+private const val REVIEWER_ON_MANUSCRIPT = "'REVIEWER_ON_MANUSCRIPT'"
+
+
+interface InviteRepository: Repository<Invite, Int> {
+    @Query("SELECT * FROM $INVITE WHERE $TARGET = $ADMIN")
+    fun allAdmin(): List<Account>
 }
