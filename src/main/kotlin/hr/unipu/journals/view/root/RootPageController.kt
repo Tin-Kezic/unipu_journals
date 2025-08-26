c433fa09739980c68315092d6db57209a33318f2
Tin Kežić <kezic.tin@gmail.com>
2025-08-26 23:32:40 +0200

    implemented password-change for root

---
+++ src/main/kotlin/hr/unipu/journals/view/root/RootPageController.kt
@@ -0,0 +1,48 @@
+package hr.unipu.journals.view.root
+
+import hr.unipu.journals.feature.account.AccountRepository
+import hr.unipu.journals.feature.invite.InvitationTarget
+import hr.unipu.journals.feature.invite.InviteRepository
+import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_ROOT
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Controller
+@RequestMapping("/root")
+class RootPageController(
+    private val passwordEncoder: PasswordEncoder,
+    private val inviteRepository: InviteRepository,
+    private val accountRepository: AccountRepository,
+) {
+    @GetMapping
+    @PreAuthorize(AUTHORIZATION_SERVICE_IS_ROOT)
+    fun page(model: Model): String {
+        model["admins"] = (accountRepository.allAdmin() + inviteRepository.allAdmin()).map { account -> AdminDTO(account.id, account.email) }
+        return "configure/root-page"
+    }
+
+    @PostMapping("/update-password")
+    @PreAuthorize(AUTHORIZATION_SERVICE_IS_ROOT)
+    fun updatePassword(@RequestParam password: String, @RequestParam passwordConfirmation: String): String {
+        if(password != passwordConfirmation) return "redirect:/root?password-mismatch"
+        accountRepository.updateRootPassword(passwordEncoder.encode(password))
+        return "redirect:/root?successfully-updated-password"
+    }
+
+    @DeleteMapping("/revoke-admin")
+    fun revokeAdmin(@RequestParam email: String): ResponseEntity<String> {
+        if(accountRepository.isAdmin(email)) accountRepository.updateIsAdmin(email, false)
+        else if (inviteRepository.isAdmin(email)) inviteRepository.revoke(email, InvitationTarget.ADMIN)
+        else return ResponseEntity.status(404).body("No admin entry found for email: $email")
+        return ResponseEntity.ok("Successfully revoked admin privileges for $email")
+    }
+}
