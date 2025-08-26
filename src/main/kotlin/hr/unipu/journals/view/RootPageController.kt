9342c81ffda31bcafdafee2222ef3809981a6f96
Tin Kežić <kezic.tin@gmail.com>
2025-08-26 14:35:54 +0200

    removed redundant import

--- src/main/kotlin/hr/unipu/journals/view/RootPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/RootPageController.kt
@@ -1,7 +1,6 @@
 package hr.unipu.journals.view
 
 import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_ROOT
-import hr.unipu.journals.security.AuthorizationService
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
