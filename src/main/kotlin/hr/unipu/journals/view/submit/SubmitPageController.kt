9f72516154611ed9d975296fb0cf68d790218f73
Tin Kežić <kezic.tin@gmail.com>
2025-08-28 22:36:53 +0200

    added basic controller to /submit

--- src/main/kotlin/hr/unipu/journals/view/submit/SubmitPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/submit/SubmitPageController.kt
@@ -1,4 +1,10 @@
 package hr.unipu.journals.view.submit
 
-class SubmitPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class SubmitPageController() {
+    @GetMapping("/submit")
+    fun page() = "submit/submit-page"
 }
