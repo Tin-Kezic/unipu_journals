ac5f51e85f58d047aee1e6adc067ce196a06ed85
Tin Kežić <kezic.tin@gmail.com>
2025-09-06 01:19:53 +0200

    added modifying import to ManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptRepository.kt
@@ -1,5 +1,6 @@
 package hr.unipu.journals.feature.manuscript
 
+import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
