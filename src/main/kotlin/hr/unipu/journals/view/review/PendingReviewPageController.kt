00eb5a2a85294d4a5a1f7a89a22fb265d16962a1
annabissinger <anna.bissinger8779@gmail.com>
2025-09-06 16:42:43 +0200

    Added functionality on pending-review-page.mustache.

--- src/main/kotlin/hr/unipu/journals/view/review/PendingReviewPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/review/PendingReviewPageController.kt
@@ -1,28 +1,48 @@
 package hr.unipu.journals.view.review
 
+import hr.unipu.journals.feature.account_role_on_manuscript.AccountRoleOnManuscriptRepository
 import hr.unipu.journals.feature.manuscript.ManuscriptRepository
 import hr.unipu.journals.feature.publication.PublicationRepository
+import hr.unipu.journals.view.home.ManuscriptDTO
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import java.time.format.DateTimeFormatter
 
 @Controller
 class PendingReviewPageController(
     private val publicationRepository: PublicationRepository,
-    private val manuscriptRepository: ManuscriptRepository
+    private val manuscriptRepository: ManuscriptRepository,
+    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
 ) {
     @GetMapping("/review")
     fun all(model: Model): String {
         model["publications-sidebar"] = publicationRepository.allUnderReview()
-        model["manuscripts"] = manuscriptRepository.allUnderReview()
+        model["manuscripts"] = manuscriptRepository.allUnderReview().map { manuscript ->
+            ManuscriptDTO(
+                id = manuscript.id,
+                title = manuscript.title,
+                authors = accountRoleOnManuscriptRepository.authors(manuscript.id),
+                publicationDate = manuscript.publicationDate?.format(DateTimeFormatter.ofPattern("dd MMM YYYY")) ?: "no publication date",
+                description = manuscript.description
+            )
+        }
         return "review/pending-review-page"
     }
     @GetMapping("review/from-publication/{publicationId}")
     fun underPublication(@PathVariable publicationId: Int, model: Model): String {
         model["publications-sidebar"] = publicationRepository.allPublished()
-        model["sections"] = manuscriptRepository.allUnderReviewByPublication(publicationId)
+        model["manuscripts"] = manuscriptRepository.allUnderReviewByPublication(publicationId).map { manuscript ->
+            ManuscriptDTO(
+                id = manuscript.id,
+                title = manuscript.title,
+                authors = accountRoleOnManuscriptRepository.authors(manuscript.id),
+                publicationDate = manuscript.publicationDate?.format(DateTimeFormatter.ofPattern("dd MMM YYYY")) ?: "no publication date",
+                description = manuscript.description
+            )
+        }
         return "review/pending-review-page"
     }
 }
