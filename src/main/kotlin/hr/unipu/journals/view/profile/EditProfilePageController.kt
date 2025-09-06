5efee93c42bd1cb452e1e78f9140c759ff895acc
Tin Kežić <kezic.tin@gmail.com>
2025-09-06 22:32:16 +0200

    fixed EditProfilePageController.kt

--- src/main/kotlin/hr/unipu/journals/view/profile/EditProfilePageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/profile/EditProfilePageController.kt
@@ -4,6 +4,8 @@ import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_ACCOUNT_OWNER_OR_ADMI
 import hr.unipu.journals.security.AuthorizationService
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
@@ -11,18 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable
 class EditProfilePageController(private val authorizationService: AuthorizationService) {
     @GetMapping("/profile/{accountId}/edit")
     @PreAuthorize(AUTHORIZATION_SERVICE_IS_ACCOUNT_OWNER_OR_ADMIN)
-    fun page(@PathVariable accountId: Int): String {
-        val account = authorizationService.account ?: throw IllegalStateException("account with id $accountId not found")
-        return """redirect:/profile/$accountId/edit
-           &fullName=${account.fullName}
-           &title=${account.title}
-           &email=${account.email}
-           &affiliation=${account.affiliation}
-           &jobType=${account.jobType}
-           &country=${account.country}
-           &city=${account.city}
-           &address=${account.address}
-           &zipCode=${account.zipCode}
-        """
+    fun page(@PathVariable accountId: Int, model: Model): String {
+        model["account"] = authorizationService.account ?: throw IllegalStateException("account with id $accountId not found")
+        return "profile/edit-profile-page"
     }
 }
