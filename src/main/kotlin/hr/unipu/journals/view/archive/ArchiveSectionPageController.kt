7918e1530c04cf7a114a15073c92835fec5d1943
Tin Kežić <kezic.tin@gmail.com>
2025-09-06 16:55:29 +0200

    implemented ArchiveSectionPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/archive/ArchiveSectionPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/archive/ArchiveSectionPageController.kt
@@ -1,13 +1,28 @@
 package hr.unipu.journals.view.archive
 
+import hr.unipu.journals.feature.publication.PublicationRepository
+import hr.unipu.journals.feature.section.SectionRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/archive/publication")
-class ArchiveSectionPageController() {
-    @GetMapping("/{publicationId}")
-    fun page(@PathVariable publicationId: Int) = "placeholder.html"
+class ArchiveSectionPageController(
+    private val publicationRepository: PublicationRepository,
+    private val sectionRepository: SectionRepository
+) {
+    @GetMapping("archive/publication/{publicationId}")
+    fun page(@PathVariable publicationId: Int, model: Model): String {
+        model["publicationsSidebar"] = publicationRepository.allArchived()
+        model["currentPublication"] = publicationRepository.title(publicationId)
+        model["sections"] = sectionRepository.allArchivedByPublicationId(publicationId).map { section ->
+            ArchiveContainerDTO(
+                id = section.id,
+                title = section.title,
+            )
+        }
+        return "archive/archive-section-page"
+    }
 }
