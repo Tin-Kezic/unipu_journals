c00a68be6947f2c0a7ecda24ef9fdc32f1a5594a
Tin Kežić <kezic.tin@gmail.com>
2025-09-06 20:18:10 +0200

    added ProfileManuscriptDTO.kt

---
+++ src/main/kotlin/hr/unipu/journals/view/profile/ProfileManuscriptDTO.kt
@@ -0,0 +1,8 @@
+package hr.unipu.journals.view.profile
+
+data class ProfileManuscriptDTO (
+    val id: Int,
+    val title: String,
+    val publicationDate: String,
+    val description: String,
+)
