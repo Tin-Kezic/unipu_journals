23eaddefd2e04138dbadc0e40744d256eed6e0fc
Tin Kežić <kezic.tin@gmail.com>
2025-09-06 16:55:43 +0200

    implemented ArchiveManuscriptPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/archive/ArchiveManuscriptPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/archive/ArchiveManuscriptPageController.kt
@@ -1,13 +1,42 @@
 package hr.unipu.journals.view.archive
 
+import hr.unipu.journals.feature.account_role_on_manuscript.AccountRoleOnManuscriptRepository
+import hr.unipu.journals.feature.manuscript.ManuscriptRepository
+import hr.unipu.journals.feature.section.SectionRepository
+import hr.unipu.journals.view.home.ManuscriptDTO
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.format.DateTimeFormatter
 
 @Controller
-@RequestMapping("/archive/publication")
-class ArchiveManuscriptPageController() {
-    @GetMapping("/{publicationId}/section/{sectionId}/")
-    fun page(@PathVariable publicationId: Int, @PathVariable sectionId: Int) = "placeholder.html"
+@RequestMapping("archive/publication")
+class ArchiveManuscriptPageController(
+    private val sectionRepository: SectionRepository,
+    private val manuscriptRepository: ManuscriptRepository,
+    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
+) {
+    @GetMapping("/{publicationId}/section/{sectionId}")
+    fun page(
+        @PathVariable publicationId: Int,
+        @PathVariable sectionId: Int,
+        model: Model
+    ): String {
+        model["sectionsSidebar"] = sectionRepository.allArchivedByPublicationId(publicationId)
+        model["manuscripts"] = manuscriptRepository.allArchivedBySectionId(sectionId).map { manuscript ->
+            ManuscriptDTO(
+                id = manuscript.id,
+                title = manuscript.title,
+                authors = accountRoleOnManuscriptRepository.authors(manuscript.id),
+                publicationDate = manuscript.publicationDate?.format(DateTimeFormatter.ofPattern("dd MMM YYYY"))
+                    ?: "no publication date",
+                description = manuscript.description
+            )
+        }
+        model["currentSection"] = sectionRepository.title(sectionId)
+        return "archive/archive-manuscript-page"
+    }
 }
