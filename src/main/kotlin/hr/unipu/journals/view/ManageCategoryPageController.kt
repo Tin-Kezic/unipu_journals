ab703cb9b199edfa351366b9edc7dbc83ef804ab
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 22:28:45 +0200

    added categories model to ManageCategoryPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/ManageCategoryPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/ManageCategoryPageController.kt
@@ -1,10 +1,16 @@
 package hr.unipu.journals.view
 
+import hr.unipu.journals.feature.category.CategoryRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class ManageCategoryPageController() {
+class ManageCategoryPageController(private val categoryRepository: CategoryRepository) {
     @GetMapping("/category")
-    fun page() = "manage/manage-category"
+    fun page(model: Model): String {
+        model["categories"] = categoryRepository.all()
+        return "manage/manage-category"
+    }
 }
