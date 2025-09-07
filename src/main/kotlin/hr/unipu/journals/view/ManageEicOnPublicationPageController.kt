24a9a9eea798d76cf6aaa70f185fc01f138ebe64
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 23:27:24 +0200

    implemented ManageEicOnPublicationPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/ManageEicOnPublicationPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/ManageEicOnPublicationPageController.kt
@@ -1,10 +1,23 @@
 package hr.unipu.journals.view
 
+import hr.unipu.journals.feature.eic_on_publication.EicOnPublicationRepository
+import hr.unipu.journals.feature.publication.PublicationRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class ManageEicOnPublicationPageController() {
+class ManageEicOnPublicationPageController(
+    private val publicationRepository: PublicationRepository,
+    private val eicOnPublicationRepository: EicOnPublicationRepository
+) {
     @GetMapping("/publication/{publicationId}/manage-eic-on-publication")
-    fun page() = "manage/manage-eic-on-publication"
+    fun page(@PathVariable publicationId: Int, model: Model): String {
+        model["currentPublication"] = publicationRepository.title(publicationId)
+        model["eicEmails"] = eicOnPublicationRepository.eicEmailsByPublicationId(publicationId)
+        return "manage/manage-eic-on-publication-page"
+    }
 }
