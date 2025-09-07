4347f1a887422f938411e3c46f2528718ab9bf09
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 23:58:09 +0200

    fixed revoke and assign query methods in EicOnPublicationController.kt

--- src/main/kotlin/hr/unipu/journals/feature/eic_on_publication/EicOnPublicationController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/eic_on_publication/EicOnPublicationController.kt
@@ -3,9 +3,8 @@ package hr.unipu.journals.feature.eic_on_publication
 import hr.unipu.journals.feature.account.AccountRepository
 import hr.unipu.journals.feature.invite.InvitationTarget
 import hr.unipu.journals.feature.invite.InviteRepository
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,8 +16,8 @@ class EicOnPublicationController(
     private val accountRepository: AccountRepository,
     private val inviteRepository: InviteRepository
 ) {
-    @PostMapping("{publicationId}/assign")
-    fun insert(@PathVariable publicationId: Int, @RequestParam email: String) {
+    @PutMapping("{publicationId}/assign-eic")
+    fun assign(@PathVariable publicationId: Int, @RequestParam email: String) {
         if(accountRepository.emailExists(email)) {
             val eicId = accountRepository.byEmail(email)!!.id
             eicOnPublicationRepository.assign(publicationId, eicId)
@@ -29,12 +28,12 @@ class EicOnPublicationController(
         )
     }
 
-    @DeleteMapping("{publicationId}/revoke")
-    fun delete(@PathVariable publicationId: Int, @RequestParam email: String) {
+    @PutMapping("{publicationId}/revoke-eic")
+    fun revoke(@PathVariable publicationId: Int, @RequestParam email: String) {
         if(accountRepository.emailExists(email)) {
             val eicId = accountRepository.byEmail(email)!!.id
             eicOnPublicationRepository.revoke(publicationId, eicId)
-        } else inviteRepository.insert(
+        } else inviteRepository.revoke(
             email = email,
             target = InvitationTarget.EIC_ON_PUBLICATION,
             targetId = publicationId
