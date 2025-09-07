a2a0aebae1172d098d05fdca194ccb96c7ff718e
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 21:13:24 +0200

    basic implementation of ConfigureEicOnPublicationPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/ConfigureEicOnPublicationPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/ConfigureEicOnPublicationPageController.kt
@@ -1,4 +1,10 @@
 package hr.unipu.journals.view
 
-class ConfigureEicOnPublicationPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ConfigureEicOnPublicationPageController() {
+    @GetMapping("/publication/{publicationId}/manage-eic-on-publication")
+    fun page() = "manage/manage-eic-on-publication"
 }
