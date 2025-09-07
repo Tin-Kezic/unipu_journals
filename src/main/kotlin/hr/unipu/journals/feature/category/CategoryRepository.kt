a475fab2547d0d28001fc2b81b9492623dc89b9e
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 22:30:36 +0200

    hotfix

--- src/main/kotlin/hr/unipu/journals/feature/category/CategoryRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/category/CategoryRepository.kt
@@ -9,7 +9,7 @@ private const val CATEGORY = "category"
 private const val ID = "id"
 private const val NAME = "name"
 interface CategoryRepository: Repository<Category, Int> {
-    @Query("SELECT * FROM $CATEGORY")
+    @Query("SELECT $NAME FROM $CATEGORY")
     fun all(): List<Category>
 
     @Modifying
