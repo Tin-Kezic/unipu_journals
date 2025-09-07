6433d3df645a25386375e911581dac4463c86678
Tin Kežić <kezic.tin@gmail.com>
2025-09-07 23:12:07 +0200

    added asign and revoke query methods to EicOnPublicationRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/eic_on_publication/EicOnPublicationRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/eic_on_publication/EicOnPublicationRepository.kt
@@ -1,6 +1,6 @@
 package hr.unipu.journals.feature.eic_on_publication
 
-import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
@@ -10,6 +10,19 @@ private const val ID = "id"
 private const val PUBLICATION_ID = "publication_id"
 private const val EIC_ID = "eic_id"
 interface EicOnPublicationRepository: Repository<EicOnPublication, Int> {
+
     @Query("SELECT EXISTS (SELECT 1 FROM $EIC_ON_PUBLICATION WHERE $PUBLICATION_ID = :$PUBLICATION_ID AND $EIC_ID = :$EIC_ID)")
     fun isEicOnPublication(@Param(EIC_ID) eicId: Int, @Param(PUBLICATION_ID) publicationId: Int): Boolean
+
+    @Modifying
+    @Query("INSERT INTO $EIC_ON_PUBLICATION ($PUBLICATION_ID, $EIC_ID) VALUES (:$PUBLICATION_ID, :$EIC_ID)")
+    fun assign(@Param(PUBLICATION_ID) publicationId: Int, @Param(EIC_ID) eicId: Int)
+
+    @Modifying
+    @Query("""
+        DELETE FROM $EIC_ON_PUBLICATION WHERE
+        WHERE $PUBLICATION_ID = :$PUBLICATION_ID
+        AND $EIC_ID = :$EIC_ID
+    """)
+    fun revoke(@Param(PUBLICATION_ID) publicationId: Int, @Param(EIC_ID) eicId: Int)
 }
