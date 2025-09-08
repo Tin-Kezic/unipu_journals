a66fad825518bdfbb2c13df36cc777cb153f63e8
Tin Kežić <kezic.tin@gmail.com>
2025-09-08 17:18:19 +0200

    added GlobalExceptionHandler.kt

---
+++ src/main/kotlin/hr/unipu/journals/view/GlobalExceptionHandler.kt
@@ -0,0 +1,26 @@
+package hr.unipu.journals.view
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun notFound() = "redirect:/404.html"
+
+    @ExceptionHandler(AccessDeniedException::class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    fun forbidden() = "redirect:/403.html"
+
+    @ExceptionHandler(InternalServerErrorException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun internalServerError() = "redirect:/505.html"
+
+}
+
+class ResourceNotFoundException(message: String) : RuntimeException(message)
+class AccessDeniedException(message: String) : RuntimeException(message)
+class InternalServerErrorException(message: String) : RuntimeException(message)
