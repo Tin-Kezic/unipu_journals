e5b84a735e90b16068c740c49ebecce64f21b401
Tin Kežić <kezic.tin@gmail.com>
2025-09-09 00:07:57 +0200

    implemented basic ReviewRoundInitializationPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/review/ReviewRoundInitializationPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/review/ReviewRoundInitializationPageController.kt
@@ -1,4 +1,10 @@
 package hr.unipu.journals.view.review
 
-class ReviewRoundInitializationPageController {
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ReviewRoundInitializationPageController() {
+    @GetMapping("/review/manuscript/{manuscriptReviewId}/round-initialization")
+    fun page() = "manage/manage-round-initialization-page"
 }
