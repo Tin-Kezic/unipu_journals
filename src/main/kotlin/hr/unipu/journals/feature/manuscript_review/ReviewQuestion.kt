7ca518eb4d78fc5f87e9efe10c1ed5152f293f77
Tin Kežić <kezic.tin@gmail.com>
2025-09-09 15:08:17 +0200

    created ReviewQuestion.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript_review/ReviewQuestion.kt
@@ -0,0 +1,9 @@
+package hr.unipu.journals.feature.manuscript_review
+
+enum class ReviewQuestion {
+    SUFFICIENT_BACKGROUND,
+    APPROPRIATE_RESEARCH_DESIGN,
+    ADEQUATELY_DESCRIBED,
+    CLEARLY_PRESENTED,
+    SUPPORTED_CONCLUSIONS
+}
