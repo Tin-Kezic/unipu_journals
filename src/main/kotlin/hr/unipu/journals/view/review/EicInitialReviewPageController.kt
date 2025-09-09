393c2c49686fdaa4c0b08534f22b753adec88279
Tin Kežić <kezic.tin@gmail.com>
2025-09-09 04:13:29 +0200

    simplified EicInitialReviewPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/review/EicInitialReviewPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/review/EicInitialReviewPageController.kt
@@ -2,11 +2,9 @@ package hr.unipu.journals.view.review
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/review")
 class EicInitialReviewPageController() {
-    @GetMapping("/{manuscriptId}/eic-initial")
+    @GetMapping("/review/{manuscriptId}/eic-initial")
     fun page() = "review/eic-initial-review-page"
 }
