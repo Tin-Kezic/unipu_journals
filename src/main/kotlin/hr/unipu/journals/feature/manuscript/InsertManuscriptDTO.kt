ec1db3c6c000a3d0aab78e6092d4d0906505d4f3
Tin Kežić <kezic.tin@gmail.com>
2025-09-11 15:12:11 +0200

    deleted InsertManuscriptDTO.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/InsertManuscriptDTO.kt
+++
@@ -1,8 +0,0 @@
-package hr.unipu.journals.feature.manuscript
-
-data class InsertManuscriptDTO (
-    val title: String,
-    val authorId: Int,
-    val categoryId: Int,
-    val fileUrl: String
-)
