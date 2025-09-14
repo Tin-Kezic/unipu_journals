3a97805da0310ad086555392299b20f6353c6acc
Tin Kežić <kezic.tin@gmail.com>
2025-09-15 00:56:03 +0200

    moved byId query method in AccountRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/account/AccountRepository.kt
@@ -21,9 +21,6 @@ private const val IS_ADMIN = "is_admin"
 
 interface AccountRepository: Repository<Account, Int> {
 
-    @Query("SELECT * FROM $ACCOUNT WHERE $ID = :$ID")
-    fun byId(@Param(ID) id: Int): Account?
-
     @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT WHERE $EMAIL = :$EMAIL AND $IS_ADMIN = TRUE)")
     fun isAdmin(@Param(EMAIL) email: String): Boolean
 
@@ -40,6 +37,9 @@ interface AccountRepository: Repository<Account, Int> {
 
     @Query("SELECT EXISTS (SELECT 1 FROM $ACCOUNT WHERE $EMAIL = :$EMAIL)")
     fun emailExists(@Param(EMAIL) email: String): Boolean
+
+    @Query("SELECT * FROM $ACCOUNT WHERE $ID = :$ID")
+    fun byId(@Param(ID) id: Int): Account?
 
     @Query("SELECT * FROM $ACCOUNT WHERE $EMAIL = :$EMAIL")
     fun byEmail(@Param(EMAIL) email: String): Account?
