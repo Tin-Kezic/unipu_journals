6e3d8543d66cf85c9b9977e60c5aa19db0a1070b
Tin Kežić <kezic.tin@gmail.com>
2025-10-16 22:32:26 +0200

    fixed updateState method in ManuscriptController.kt preventing illegal action

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/ManuscriptController.kt
@@ -1,6 +1,7 @@
 package hr.unipu.journals.feature.manuscript
 
 import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_SECTION_EDITOR_ON_SECTION_OR_SUPERIOR
+import hr.unipu.journals.security.AuthorizationService
 import hr.unipu.journals.view.submit.AuthorDTO
 import org.jsoup.Jsoup
 import org.jsoup.safety.Safelist
@@ -16,7 +17,10 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api/publication")
-class ManuscriptController(private val publicationRepository: ManuscriptRepository) {
+class ManuscriptController(
+    private val manuscriptRepository: ManuscriptRepository,
+    private val authorizationService: AuthorizationService
+) {
     @PostMapping("{publicationId}/section/{sectionId}/insert")
     fun insert(
         @PathVariable sectionId: Int,
@@ -39,16 +43,24 @@ class ManuscriptController(private val publicationRepository: ManuscriptReposito
         return ResponseEntity.ok("manuscript successfully added")
     }
     @PutMapping("/{publicationId}/section/{sectionId}/manuscript/{manuscriptId}/update-state")
-    @PreAuthorize(AUTHORIZATION_SERVICE_IS_SECTION_EDITOR_ON_SECTION_OR_SUPERIOR)
     fun updateState(
         @PathVariable publicationId: Int,
         @PathVariable sectionId: Int,
         @PathVariable manuscriptId: Int,
         @RequestParam newState: ManuscriptState
     ): ResponseEntity<String> {
-        return if(publicationRepository.exists(manuscriptId)) {
-            publicationRepository.updateState(manuscriptId, newState)
-            return ResponseEntity.ok("manuscript isHidden successfully updated")
-        } else ResponseEntity.badRequest().body("manuscript with id: $manuscriptId does not exist")
+        val manuscript = manuscriptRepository.byId(manuscriptId) ?: return ResponseEntity.badRequest().body("failed to find manuscript with id $manuscriptId")
+        if(newState !in listOf(ManuscriptState.HIDDEN, ManuscriptState.ARCHIVED)) return ResponseEntity.badRequest().body("cannot manually change manuscript state to $newState")
+        if(newState == ManuscriptState.ARCHIVED) {
+            if(authorizationService.isSectionEditorOnSectionOrSuperior(publicationId, sectionId)) return ResponseEntity.status(403).body("unauthorized to archive manuscripts in section $sectionId")
+            if(manuscript.state != ManuscriptState.PUBLISHED) return ResponseEntity.badRequest().body("cannot archive manuscript that is not published")
+        }
+        if(newState == ManuscriptState.HIDDEN) {
+            if(authorizationService.isAdmin()) return ResponseEntity.status(403).body("unauthorized to hide manuscripts")
+            if(manuscript.state !in listOf(ManuscriptState.PUBLISHED, ManuscriptState.REJECTED)) return ResponseEntity.badRequest().body("cannot hide manuscript that is not published or rejected")
+        }
+        val rowsAffected = manuscriptRepository.updateState(manuscriptId, newState)
+        return if(rowsAffected == 1) ResponseEntity.ok("successfully updated state on manuscript: $manuscriptId")
+        else ResponseEntity.internalServerError().body("failed updating state on manuscript: $manuscriptId")
     }
 }
