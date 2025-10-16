00ccc3a580cc2d148355039a9f92163c310644a7
Tin Kežić <kezic.tin@gmail.com>
2025-10-16 14:51:47 +0200

    improved SectionEditorOnSectionController.kt

--- src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section_editor_on_section/SectionEditorOnSectionController.kt
@@ -3,6 +3,7 @@ package hr.unipu.journals.feature.section_editor_on_section
 import hr.unipu.journals.feature.account.AccountRepository
 import hr.unipu.journals.feature.invite.InvitationTarget
 import hr.unipu.journals.feature.invite.InviteRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,28 +21,30 @@ class SectionEditorOnSectionController(
     fun assign(
         @PathVariable sectionId: Int,
         @RequestParam email: String
-    ) {
-        if(accountRepository.existsByEmail(email)) {
-            val sectionEditorId = accountRepository.byEmail(email)!!.id
-            sectionEditorOnSectionRepository.assign(sectionId, sectionEditorId)
-        } else inviteRepository.invite(
+    ): ResponseEntity<String> {
+        val rowsAffected = accountRepository.byEmail(email)?.let {
+            sectionEditorOnSectionRepository.assign(sectionId, it.id)
+        } ?: inviteRepository.invite(
             email = email,
             target = InvitationTarget.SECTION_EDITOR_ON_SECTION,
             targetId = sectionId
         )
+        return if(rowsAffected > 0) ResponseEntity.ok("successfully assigned section editor $email on section $sectionId")
+        else ResponseEntity.internalServerError().body("failed assigning section editor $email on section $sectionId")
     }
     @PutMapping("{publicationId}/section/{sectionId}/revoke-section-editor")
     fun revoke(
         @PathVariable sectionId: Int,
         @RequestParam email: String
-    ) {
-        if(accountRepository.existsByEmail(email)) {
-            val sectionEditorId = accountRepository.byEmail(email)!!.id
-            sectionEditorOnSectionRepository.revoke(sectionId, sectionEditorId)
-        } else inviteRepository.revoke(
+    ): ResponseEntity<String> {
+        val rowsAffected = accountRepository.byEmail(email)?.let {
+            sectionEditorOnSectionRepository.revoke(sectionId, it.id)
+        } ?: inviteRepository.revoke(
             email = email,
             target = InvitationTarget.SECTION_EDITOR_ON_SECTION,
             targetId = sectionId
         )
+        return if(rowsAffected > 0) ResponseEntity.ok("successfully revoked section editor $email on section $sectionId")
+        else ResponseEntity.internalServerError().body("failed revoking section editor $email on section $sectionId")
     }
 }
