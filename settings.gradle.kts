5de00633b1341bd3a55fb2d181454971a79894ec
Tin Kežić <kezic.tin@gmail.com>
2025-10-23 22:22:34 +0200

    added comment in settings.gradle.kts in case of migrating to multi-module architecture

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "unipu_journals"
+/*
+include()
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+    repositories {
+        mavenCentral()
+    }
+}
+*/
