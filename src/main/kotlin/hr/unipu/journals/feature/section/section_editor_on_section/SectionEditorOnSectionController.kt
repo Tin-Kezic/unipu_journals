7fd109ea4afc5b12293caba16f0c645465a8016f
Tin Kežić <kezic.tin@gmail.com>
2025-10-26 01:03:07 +0200

    added authorization to SectionEditorOnSectionController.kt

--- src/main/kotlin/hr/unipu/journals/feature/section/section_editor_on_section/SectionEditorOnSectionController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/section/section_editor_on_section/SectionEditorOnSectionController.kt
@@ -3,7 +3,10 @@ package hr.unipu.journals.feature.section.section_editor_on_section
 import hr.unipu.journals.feature.account.AccountRepository
 import hr.unipu.journals.feature.invite.InvitationTarget
 import hr.unipu.journals.feature.invite.InviteRepository
+import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_EIC_ON_PUBLICATION_OR_SUPERIOR
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,7 +21,9 @@ class SectionEditorOnSectionController(
     private val inviteRepository: InviteRepository
 ) {
     @PutMapping("{publicationId}/section/{sectionId}/assign-section-editor")
+    @PreAuthorize(AUTHORIZATION_SERVICE_IS_EIC_ON_PUBLICATION_OR_SUPERIOR)
     fun assign(
+        @PathVariable publicationId: Int,
         @PathVariable sectionId: Int,
         @RequestParam email: String
     ): ResponseEntity<String> {
@@ -32,9 +37,10 @@ class SectionEditorOnSectionController(
         return if(rowsAffected == 1) ResponseEntity.ok("successfully assigned section editor $email on section $sectionId")
         else ResponseEntity.internalServerError().body("failed to assign section editor $email on section $sectionId")
     }
-    @PutMapping("{publicationId}/section/{sectionId}/revoke-section-editor")
     @DeleteMapping("{publicationId}/section/{sectionId}/revoke-section-editor")
+    @PreAuthorize(AUTHORIZATION_SERVICE_IS_EIC_ON_PUBLICATION_OR_SUPERIOR)
     fun revoke(
+        @PathVariable publicationId: Int,
         @PathVariable sectionId: Int,
         @RequestParam email: String
     ): ResponseEntity<String> {
