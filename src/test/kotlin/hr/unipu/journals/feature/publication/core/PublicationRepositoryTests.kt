7d35555513464c1c8c116d6652c54c6fcfd230bb
Tin Kežić <kezic.tin@gmail.com>
2025-10-28 09:57:00 +0100

    changes

--- src/test/kotlin/hr/unipu/journals/feature/publication/core/PublicationRepositoryTests.kt
+++ src/test/kotlin/hr/unipu/journals/feature/publication/core/PublicationRepositoryTests.kt
@@ -13,8 +13,7 @@ class PublicationRepositoryTests {
     @Autowired private lateinit var publicationRepository: PublicationRepository
     @Autowired private lateinit var jdbcTemplate: JdbcTemplate
 
-    @Test
-    fun `retrieve all publications which contain pending manuscripts by account id`() {
+    @Test fun `retrieve all publications which contain pending manuscripts by account id`() {
         val publications = listOf(
             Publication(1, "Journal of AI Research", false),
             Publication(2, "Nature of Biology", false),
@@ -24,12 +23,10 @@ class PublicationRepositoryTests {
         assertEquals(publications.takeLast(2), publicationRepository.allWithPendingManuscripts(accountId = 26))
         assertEquals(publications.takeLast(1), publicationRepository.allWithPendingManuscripts(accountId = 27))
     }
-    @Test
-    fun `retrieve publication title by publication id`() {
+    @Test fun `retrieve publication title by publication id`() {
         assertEquals("Journal of AI Research", publicationRepository.title(1))
     }
-    @Test
-    fun `retrieve all published publications`() {
+    @Test fun `retrieve all published publications`() {
         assertEquals(
             listOf(
                 Publication(6, "Published empty publication", false),
@@ -40,8 +37,7 @@ class PublicationRepositoryTests {
             publicationRepository.allPublished()
         )
     }
-    @Test
-    fun `retrieve all publications that are hidden or contain hidden sections or hidden manuscripts`() {
+    @Test fun `retrieve all publications that are hidden or contain hidden sections or hidden manuscripts`() {
         assertEquals(
             listOf(
                 Publication(1, "Journal of AI Research", false),
@@ -53,38 +49,32 @@ class PublicationRepositoryTests {
             publicationRepository.allHidden()
         )
     }
-    @Test
-    fun `retrieve all publications containing archived manuscripts`() {
+    @Test fun `retrieve all publications containing archived manuscripts`() {
         assertEquals(
             listOf(Publication(1, "Journal of AI Research", false)),
             publicationRepository.allContainingArchivedManuscripts()
         )
     }
-    @Test
-    fun `insert publication`() {
+    @Test fun `insert publication`() {
         Assertions.assertFalse(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE title = 'new publication')"))
         assertEquals(1, publicationRepository.insert("new publication"))
         Assertions.assertTrue(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE title = 'new publication')"))
     }
-    @Test
-    fun `update publication title`() {
+    @Test fun `update publication title`() {
         Assertions.assertFalse(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE title = 'updated publication title')"))
         assertEquals(1, publicationRepository.update(1, title = "updated publication title"))
         Assertions.assertTrue(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE title = 'updated publication title')"))
     }
-    @Test
-    fun `check if publication exists`() {
+    @Test fun `check if publication exists`() {
         Assertions.assertTrue(publicationRepository.exists(1))
         Assertions.assertFalse(publicationRepository.exists(1000))
     }
-    @Test
-    fun `update is publication hidden`() {
+    @Test fun `update is publication hidden`() {
         Assertions.assertTrue(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE id = 2 AND is_hidden = FALSE)"))
         assertEquals(1, publicationRepository.update(2, isHidden = true))
         Assertions.assertTrue(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE id = 2 AND is_hidden = TRUE)"))
     }
-    @Test
-    fun `delete publication by publication id`() {
+    @Test fun `delete publication by publication id`() {
         Assertions.assertTrue(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE id = 1)"))
         assertEquals(1, publicationRepository.delete(1))
         Assertions.assertFalse(jdbcTemplate.queryForObject<Boolean>("SELECT EXISTS (SELECT 1 FROM publication WHERE id = 1)"))
