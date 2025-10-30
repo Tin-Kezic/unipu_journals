6baf1e5d12dfd4ea7be716ee1086825c47e173ec
Tin Kežić <kezic.tin@gmail.com>
2025-10-30 23:16:47 +0100

    created Affiliation.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/publication/core/Affiliation.kt
@@ -0,0 +1,11 @@
+package hr.unipu.journals.feature.publication.core
+
+enum class Affiliation {
+    EIC_ON_PUBLICATION,
+    SECTION_EDITOR,
+    EIC_ON_MANUSCRIPT,
+    EDITOR,
+    REVIEWER,
+    CORRESPONDING_AUTHOR,
+    AUTHOR
+}
