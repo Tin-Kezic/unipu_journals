c6b245205401c08d1a3cd2ad00e5eeaa30e3b42f
Tin Kežić <kezic.tin@gmail.com>
2025-10-30 23:16:38 +0100

    created PublicationType.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/publication/core/PublicationType.kt
@@ -0,0 +1,9 @@
+package hr.unipu.journals.feature.publication.core
+
+enum class PublicationType {
+    PUBLIC,
+    HIDDEN,
+    CONTAINS_ARCHIVED_MANUSCRIPTS,
+    CONTAINS_PENDING_MANUSCRIPTS,
+    CATEGORY
+}
