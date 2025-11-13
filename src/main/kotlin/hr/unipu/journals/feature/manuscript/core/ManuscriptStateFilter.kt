ba14f835f817c505ee6370a20d98f3cd58ae4c7a
Tin Kežić <kezic.tin@gmail.com>
2025-11-13 13:56:51 +0100

    created ManuscriptStateFilter.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptStateFilter.kt
@@ -0,0 +1,13 @@
+package hr.unipu.journals.feature.manuscript.core
+
+enum class ManuscriptStateFilter {
+    ALL_AWAITING_REVIEW,
+    AWAITING_EIC_REVIEW,
+    AWAITING_EDITOR_REVIEW,
+    AWAITING_REVIEWER_REVIEW,
+    MINOR_MAJOR,
+    PUBLISHED,
+    REJECTED,
+    ARCHIVED,
+    HIDDEN
+}
