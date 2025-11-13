7ff17f12e3df6cdfffaee8715b7468df5d00f12d
Tin Kežić <kezic.tin@gmail.com>
2025-11-13 13:56:40 +0100

    created Sorting.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/publication/core/Sorting.kt
@@ -0,0 +1,10 @@
+package hr.unipu.journals.feature.publication.core
+
+enum class Sorting {
+    ALPHABETICAL_A_Z,
+    ALPHABETICAL_Z_A,
+    PUBLICATION_NEW,
+    PUBLICATION_OLD,
+    VIEWS,
+    DOWNLOADS
+}
