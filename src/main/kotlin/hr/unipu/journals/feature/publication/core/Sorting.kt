60b5ba0268a2ad9c252651e174d567c0d2f5cba9
Tin Kežić <kezic.tin@gmail.com>
2025-11-18 15:16:32 +0100

    modified Sorting.kt

--- src/main/kotlin/hr/unipu/journals/feature/publication/core/Sorting.kt
+++ src/main/kotlin/hr/unipu/journals/feature/publication/core/Sorting.kt
@@ -3,7 +3,7 @@ package hr.unipu.journals.feature.publication.core
 enum class Sorting {
     ALPHABETICAL_A_Z,
     ALPHABETICAL_Z_A,
-    PUBLICATION_NEW,
-    PUBLICATION_OLD,
+    NEWEST,
+    OLDEST,
     VIEWS
 }
