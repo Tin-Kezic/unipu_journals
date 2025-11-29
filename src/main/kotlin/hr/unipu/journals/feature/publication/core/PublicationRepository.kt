62296d456194ee131b9d259faf3887795ae7b9bb
Tin Kežić <kezic.tin@gmail.com>
2025-11-29 01:40:16 +0100

    modified `all` query method in PublicationRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/publication/core/PublicationRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/publication/core/PublicationRepository.kt
@@ -18,7 +18,7 @@ interface PublicationRepository: Repository<Publication, Int> {
         LEFT JOIN eic_on_publication ON publication.id = eic_on_publication.publication_id AND :affiliation IS NOT NULL
         LEFT JOIN section_editor_on_section ON publication_section.id = section_editor_on_section.publication_section_id AND :affiliation IS NOT NULL
         LEFT JOIN account_role_on_manuscript ON manuscript.id = account_role_on_manuscript.manuscript_id
-        LEFT JOIN account on :account_id = account.id
+        LEFT JOIN account ON :account_id = account.id
         WHERE (category.name = :category OR :category IS NULL)
         AND (
             :manuscript_state_filter = 'HIDDEN' AND (
