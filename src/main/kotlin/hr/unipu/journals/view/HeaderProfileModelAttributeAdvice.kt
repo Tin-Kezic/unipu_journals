6ae3c495ab9b7e8792e3a56ff9e627893131d235
Tin Kežić <kezic.tin@gmail.com>
2025-12-08 06:54:54 +0100

    removed PendingReviewPageController.kt from HeaderProfileModelAttributeAdvice.kt

--- src/main/kotlin/hr/unipu/journals/view/HeaderProfileModelAttributeAdvice.kt
+++ src/main/kotlin/hr/unipu/journals/view/HeaderProfileModelAttributeAdvice.kt
@@ -5,7 +5,6 @@ import hr.unipu.journals.view.home.PublicationPageController
 import hr.unipu.journals.view.profile.EditProfilePageController
 import hr.unipu.journals.view.profile.ProfilePageController
 import hr.unipu.journals.view.review.EicInitialReviewPageController
-import hr.unipu.journals.view.review.PendingReviewPageController
 import hr.unipu.journals.view.review.ReviewPageController
 import hr.unipu.journals.view.review.ReviewRoundInitializationPageController
 import hr.unipu.journals.view.submit.SubmitPageController
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.ModelAttribute
     EditProfilePageController::class,
     ProfilePageController::class,
     EicInitialReviewPageController::class,
-    PendingReviewPageController::class,
     ReviewPageController::class,
     ReviewRoundInitializationPageController::class,
     SubmitPageController::class,
