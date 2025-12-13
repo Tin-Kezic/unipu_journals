7791ac62369ec045e040c2a250448a0f98b5a18a
Tin Kežić <kezic.tin@gmail.com>
2025-12-13 19:54:04 +0100

    hotfix

--- src/main/kotlin/hr/unipu/journals/feature/invite/InvitationTarget.kt
+++ src/main/kotlin/hr/unipu/journals/feature/invite/InvitationTarget.kt
@@ -3,8 +3,8 @@ package hr.unipu.journals.feature.invite
 enum class InvitationTarget {
     ADMIN,
     EIC_ON_PUBLICATION,
-    EIC_ON_MANUSCRIPT,
     SECTION_EDITOR,
+    EIC_ON_MANUSCRIPT,
     EDITOR,
     REVIEWER
 }
