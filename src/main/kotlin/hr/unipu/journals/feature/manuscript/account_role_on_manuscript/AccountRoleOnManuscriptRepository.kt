b3e97ce6d7e66f58b524ae2231d1c2857bf7fe11
Tin Kežić <kezic.tin@gmail.com>
2025-12-14 09:38:01 +0100

    bugfix

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -5,8 +5,9 @@ import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
 interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript, Int> {
-    @Query("SELECT account_role FROM account_role_on_manuscript WHERE account_id = :account_id AND target_id = :target_id")
-    fun role(@Param("account_id") accountId: Int, @Param("target_id") targetId: Int): List<ManuscriptRole>
+    @Query("SELECT account_role FROM account_role_on_manuscript WHERE account_id = :account_id AND manuscript_id = :manuscript_id")
+    fun role(@Param("account_id") accountId: Int, @Param("manuscript_id") manuscriptId: Int): List<ManuscriptRole>
+
     @Query("""
         SELECT DISTINCT account.full_name FROM account_role_on_manuscript
         JOIN account ON account.id = account_role_on_manuscript.account_id
@@ -15,6 +16,7 @@ interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript
         AND manuscript.id = :manuscript_id
     """)
     fun authors(@Param("manuscript_id") manuscriptId: Int): List<String>
+
     @Query("""
         SELECT DISTINCT account.full_name FROM account_role_on_manuscript
         JOIN account ON account.id = account_role_on_manuscript.account_id
@@ -23,6 +25,7 @@ interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript
         AND account_role_on_manuscript.account_role = 'CORRESPONDING_AUTHOR'
     """)
     fun correspondingAuthor(@Param("manuscript_id") manuscriptId: Int): String
+
     @Query("""
         SELECT EXISTS (SELECT 1 FROM account_role_on_manuscript
         WHERE account_role_on_manuscript.account_role = :account_role::manuscript_role
@@ -35,6 +38,7 @@ interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript
         @Param("account_id") accountId: Int,
         @Param("manuscript_id") manuscriptId: Int,
     ): Boolean
+
     @Query("INSERT INTO account_role_on_manuscript (manuscript_id, account_id, account_role) VALUES (:manuscript_id, :account_id, :account_role)")
     fun assign(
         @Param("account_role") accountRole: ManuscriptRole,
