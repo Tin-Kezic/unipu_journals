e9ce22bac806358c5f5b4d66489c334c3cbab033
Tin Kežić <kezic.tin@gmail.com>
2025-12-15 22:43:59 +0100

    created InvitedManuscript.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/InvitedManuscript.kt
@@ -0,0 +1,20 @@
+package hr.unipu.journals.feature.manuscript.core
+
+import hr.unipu.journals.feature.invite.InvitationTarget
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import java.time.LocalDateTime
+
+data class InvitedManuscript(
+    val target: InvitationTarget,
+    @Id val id: Int,
+    val title: String,
+    val description: String,
+    val categoryId: Int,
+    @Column("current_state")
+    val state: ManuscriptState,
+    val sectionId: Int,
+    val downloadUrl: String,
+    val submissionDate: LocalDateTime,
+    val publicationDate: LocalDateTime?,
+)
