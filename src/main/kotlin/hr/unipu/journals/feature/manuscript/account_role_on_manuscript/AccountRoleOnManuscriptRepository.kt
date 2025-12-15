a1a3ceeb96db4a3b262b3a81097605c3dc1886d3
Tin Kežić <kezic.tin@gmail.com>
2025-12-15 22:45:44 +0100

    modified AccountRoleOnManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -1,5 +1,8 @@
 package hr.unipu.journals.feature.manuscript.account_role_on_manuscript
 
+import hr.unipu.journals.feature.manuscript.core.AffiliatedManuscript
+import hr.unipu.journals.feature.manuscript.core.ManuscriptStateFilter
+import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
@@ -16,6 +19,39 @@ interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript
         AND manuscript.id = :manuscript_id
     """)
     fun authors(@Param("manuscript_id") manuscriptId: Int): List<String>
+
+    @Query("""
+        SELECT DISTINCT account_role_on_manuscript.account_role, manuscript.* FROM account_role_on_manuscript
+        JOIN manuscript ON account_role_on_manuscript.manuscript_id = manuscript.id
+        JOIN publication_section on manuscript.section_id = publication_section.id
+        JOIN publication on publication_section.publication_id = publication.id
+        WHERE publication_section.is_hidden = FALSE AND publication.is_hidden = FALSE
+        AND publication_section.id = :section_id
+        AND account_role_on_manuscript.account_id = :account_id
+        AND (
+            :manuscript_state_filter = 'ALL_AWAITING_REVIEW' AND (
+                account_role_on_manuscript.account_role = 'EIC' AND manuscript.current_state IN ('AWAITING_EIC_REVIEW', 'AWAITING_EDITOR_REVIEW', 'AWAITING_REVIEWER_REVIEW')
+                OR
+                account_role_on_manuscript.account_role = 'EDITOR' AND manuscript.current_state IN ('AWAITING_EDITOR_REVIEW', 'AWAITING_REVIEWER_REVIEW')
+                OR
+                account_role_on_manuscript.account_role = 'REVIEWER' AND manuscript.current_state = 'AWAITING_REVIEWER_REVIEW'
+            )
+            OR :manuscript_state_filter = 'AWAITING_EIC_REVIEW' AND (
+                manuscript.current_state = 'AWAITING_EIC_REVIEW' AND account_role_on_manuscript.account_role = 'EIC'
+            )
+            OR :manuscript_state_filter = 'AWAITING_EDITOR_REVIEW' AND (
+                manuscript.current_state = 'AWAITING_EDITOR_REVIEW' AND account_role_on_manuscript.account_role = 'EDITOR'
+            )
+            OR :manuscript_state_filter = 'AWAITING_REVIEWER_REVIEW' AND (
+                manuscript.current_state = 'AWAITING_REVIEWER_REVIEW' AND account_role_on_manuscript.account_role = 'REVIEWER'
+            )
+        )
+    """)
+    fun affiliatedManuscripts(
+        @Param("account_id") id: Int,
+        @Param("manuscript_state_filter") manuscriptStateFilter: ManuscriptStateFilter,
+        @Param("section_id") sectionId: Int,
+    ): List<AffiliatedManuscript>
 
     @Query("""
         SELECT DISTINCT account.full_name FROM account_role_on_manuscript
@@ -39,7 +75,8 @@ interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript
         @Param("manuscript_id") manuscriptId: Int,
     ): Boolean
 
-    @Query("INSERT INTO account_role_on_manuscript (manuscript_id, account_id, account_role) VALUES (:manuscript_id, :account_id, :account_role)")
+    @Modifying
+    @Query("INSERT INTO account_role_on_manuscript (manuscript_id, account_id, account_role) VALUES (:manuscript_id, :account_id, :account_role::manuscript_role)")
     fun assign(
         @Param("account_role") accountRole: ManuscriptRole,
         @Param("account_id") accountId: Int,
