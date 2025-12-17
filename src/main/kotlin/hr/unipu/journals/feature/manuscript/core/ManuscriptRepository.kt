b2ad44dadf1d5f9650138a28dccb717dbde3cd71
Tin Kežić <kezic.tin@gmail.com>
2025-12-17 10:44:22 +0100

    bugfix

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptRepository.kt
@@ -15,7 +15,7 @@ interface ManuscriptRepository: Repository<Manuscript, Int> {
         JOIN publication_section ON manuscript.section_id = publication_section.id
         JOIN publication ON publication.id = publication_section.publication_id
         LEFT JOIN category ON manuscript.category_id = category.id
-        LEFT JOIN eic_on_publication ON publication.id = eic_on_publication.publication_id AND :affiliation IS NOT NULL
+        LEFT JOIN eic_on_publication ON publication.id = eic_on_publication.publication_id
         LEFT JOIN section_editor_on_section ON publication_section.id = section_editor_on_section.publication_section_id AND :affiliation IS NOT NULL
         LEFT JOIN account_role_on_manuscript ON manuscript.id = account_role_on_manuscript.manuscript_id
         LEFT JOIN account ON :account_id = account.id
