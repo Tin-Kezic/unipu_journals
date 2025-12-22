6c8038c3025f0ea46254b6633fc8dc71414a8aa0
Tin Kežić <kezic.tin@gmail.com>
2025-12-22 12:45:05 +0100

    created UnregisteredAuthorRepository.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/unregistered_author/UnregisteredAuthorRepository.kt
@@ -0,0 +1,19 @@
+package hr.unipu.journals.feature.unregistered_author
+
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.Repository
+import org.springframework.data.repository.query.Param
+
+interface UnregisteredAuthorRepository: Repository<UnregisteredAuthor, Int> {
+    @Query("SELECT * FROM unregistered_author WHERE manuscript_id = :manuscript_id")
+    fun authors(@Param("manuscript_id") manuscriptId: Int): List<UnregisteredAuthor>
+
+    @Query("INSERT INTO unregistered_authors (full_name, email, country, affiliation, manuscript_id) VALUES (:full_name, :email, :country, :affiliation, :manuscript_id)")
+    fun insert(
+        @Param("full_name") fullName: String,
+        @Param("email") email: String,
+        @Param("country") country: String,
+        @Param("affiliation") affiliation: String,
+        @Param("manuscript_id") manuscriptId: Int,
+    )
+}
