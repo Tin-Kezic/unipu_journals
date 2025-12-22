8e2a670286edf17b471bf77c1fb868bb6173366f
Tin Kežić <kezic.tin@gmail.com>
2025-12-22 12:39:34 +0100

    annotated Invite.kt val id with Id annotation

--- src/main/kotlin/hr/unipu/journals/feature/invite/Invite.kt
+++ src/main/kotlin/hr/unipu/journals/feature/invite/Invite.kt
@@ -1,10 +1,11 @@
 package hr.unipu.journals.feature.invite
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("invite")
 data class Invite(
-    val id: Int,
+    @Id val id: Int,
     val email: String,
     val target: InvitationTarget
 )
