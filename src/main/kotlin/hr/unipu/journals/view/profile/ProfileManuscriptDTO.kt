2ac9c62c16e18dc48f9afc5d45f499e07f98ee84
Tin Kežić <kezic.tin@gmail.com>
2025-12-26 22:19:59 +0100

    deleted ProfileManuscriptDTO.kt

--- src/main/kotlin/hr/unipu/journals/view/profile/ProfileManuscriptDTO.kt
+++
@@ -1,9 +0,0 @@
-package hr.unipu.journals.view.profile
-
-data class ProfileManuscriptDTO (
-    val id: Int,
-    val title: String,
-    val authors: List<String>,
-    val publicationDate: String,
-    val description: String,
-)
