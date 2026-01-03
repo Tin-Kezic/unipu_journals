b83a94f1ec7341d966f7f6ae3c6d8e6fbb00f783
Tin Kežić <kezic.tin@gmail.com>
2026-01-03 21:17:55 +0100

    created CacheConfig.kt

---
+++ src/main/kotlin/hr/unipu/journals/CacheConfig.kt
@@ -0,0 +1,24 @@
+package hr.unipu.journals
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.cache.CacheManager
+import org.springframework.cache.caffeine.CaffeineCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
+@Configuration
+class CacheConfig {
+    @Bean
+    fun cacheManager(): CacheManager {
+        val caffeine = Caffeine.newBuilder()
+            .expireAfterWrite(15, TimeUnit.MINUTES)
+            .maximumSize(10_000)
+        val pendingRegistrations =
+            CaffeineCache("pendingRegistrations", caffeine.build())
+        return SimpleCacheManager().apply {
+            setCaches(listOf(pendingRegistrations))
+        }
+    }
+}
