0f6fde6ca38e04da9c73b4e07e132f8265700773
Tin Kežić <kezic.tin@gmail.com>
2026-01-03 21:17:27 +0100

    added enable caching annotation in JournalsApplication.kt

--- src/main/kotlin/hr/unipu/journals/JournalsApplication.kt
+++ src/main/kotlin/hr/unipu/journals/JournalsApplication.kt
@@ -2,10 +2,12 @@ package hr.unipu.journals
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableAsync
 
-@SpringBootApplication
 @EnableAsync
+@EnableCaching
+@SpringBootApplication
 class JournalsApplication
 
 fun main(args: Array<String>) {
