e1e41fe098c67c082de3a851a47c6d0a8c64889f
Tin Kežić <kezic.tin@gmail.com>
2026-01-05 00:43:46 +0100

    import change in SecurityConfig.kt

--- src/main/kotlin/hr/unipu/journals/security/SecurityConfig.kt
+++ src/main/kotlin/hr/unipu/journals/security/SecurityConfig.kt
@@ -1,6 +1,6 @@
 package hr.unipu.journals.security
 
-import hr.unipu.journals.feature.account.core.AccountRepository
+import hr.unipu.journals.feature.account.AccountRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
