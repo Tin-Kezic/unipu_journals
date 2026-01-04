670f83f66548ea3a383e5f88580b1b92d57f3923
Tin Kežić <kezic.tin@gmail.com>
2026-01-05 00:24:12 +0100

    bugfix

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscript.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscript.kt
@@ -1,7 +1,6 @@
 package hr.unipu.journals.feature.manuscript.account_role_on_manuscript
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("account_role_on_manuscript")
@@ -9,6 +8,5 @@ data class AccountRoleOnManuscript(
     @Id val id: Int,
     val manuscriptId: Int,
     val accountId: Int,
-    @Column("current_role")
     val accountRole: ManuscriptRole
 )
