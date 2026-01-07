a3c93c917f66f0bb21a88d25dc267ac1ee46afa5
Tin Kežić <kezic.tin@gmail.com>
2026-01-07 23:48:18 +0100

    deleted comment in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,1 @@
 rootProject.name = "unipu_journals"
-/*
-include()
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("libs.versions.toml"))
-        }
-    }
-    repositories {
-        mavenCentral()
-    }
-}
-*/
