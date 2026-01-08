a58ff7918f28b9dd3a3f8d748d0de87c7e4886f9
Tin Kežić <kezic.tin@gmail.com>
2026-01-08 01:17:34 +0100

    created AuthorDTO.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/AuthorDTO.kt
@@ -0,0 +1,8 @@
+package hr.unipu.journals.feature.manuscript.core
+
+data class AuthorDTO(
+    val fullName: String,
+    val email: String,
+    val country: String,
+    val affiliation: String,
+)
