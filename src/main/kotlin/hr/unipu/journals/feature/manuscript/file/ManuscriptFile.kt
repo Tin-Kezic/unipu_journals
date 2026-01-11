56223cddf61c07192e769f07f5d471d4d13db44b
Tin Kežić <kezic.tin@gmail.com>
2026-01-11 21:20:57 +0100

    added name to ManuscriptFile.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFile.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFile.kt
@@ -6,6 +6,7 @@ import org.springframework.data.relational.core.mapping.Table
 @Table("manuscript_file")
 data class ManuscriptFile(
     @Id val id: Int,
+    val name: String,
     val path: String,
     val manuscriptId: Int
 )
