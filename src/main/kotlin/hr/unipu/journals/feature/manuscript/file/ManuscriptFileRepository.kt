95c2fae4415b15522b46f90dc6ef3fa46af41057
Tin Kežić <kezic.tin@gmail.com>
2026-01-11 21:20:42 +0100

    added name to insert query method in ManuscriptFileRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFileRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFileRepository.kt
@@ -8,6 +8,10 @@ interface ManuscriptFileRepository: Repository<ManuscriptFile, Int> {
     @Query("SELECT * FROM file WHERE manuscript_id = :manuscript_id")
     fun allFilesByManuscriptId(@Param("manuscript_id") manuscriptId: Int): ManuscriptFile
 
-    @Query("INSERT INTO file (path, manuscript_id) VALUES (:path, :manuscript_id)")
-    fun insert(@Param("path") path: String, @Param("manuscript_id") manuscriptId: Int): Int
+    @Query("INSERT INTO file (name, path, manuscript_id) VALUES (:name, :path, :manuscript_id)")
+    fun insert(
+        @Param("name") name: String,
+        @Param("path") path: String,
+        @Param("manuscript_id") manuscriptId: Int
+    ): Int
 }
