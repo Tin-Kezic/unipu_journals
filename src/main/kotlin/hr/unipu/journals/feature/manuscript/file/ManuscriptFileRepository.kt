88d30a5a070a703c9d4d61f5ee15f14588e7c959
Tin Kežić <kezic.tin@gmail.com>
2026-01-13 15:57:53 +0100

    hotfix

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFileRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/file/ManuscriptFileRepository.kt
@@ -1,14 +1,16 @@
 package hr.unipu.journals.feature.manuscript.file
 
+import org.springframework.data.jdbc.repository.query.Modifying
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
 interface ManuscriptFileRepository: Repository<ManuscriptFile, Int> {
-    @Query("SELECT * FROM file WHERE manuscript_id = :manuscript_id")
+    @Query("SELECT * FROM manuscript_file WHERE manuscript_id = :manuscript_id")
     fun allFilesByManuscriptId(@Param("manuscript_id") manuscriptId: Int): List<ManuscriptFile>
 
-    @Query("INSERT INTO file (name, path, manuscript_id) VALUES (:name, :path, :manuscript_id)")
+    @Modifying
+    @Query("INSERT INTO manuscript_file (name, path, manuscript_id) VALUES (:name, :path, :manuscript_id)")
     fun insert(
         @Param("name") name: String,
         @Param("path") path: String,
