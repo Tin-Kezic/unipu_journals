81ab4d4b23b8529be474160e7a48fef75d566c20
Tin Kežić <kezic.tin@gmail.com>
2026-01-13 15:56:18 +0100

    modified ZipService.kt to take File instead of MultipartFile

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ZipService.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ZipService.kt
@@ -3,18 +3,14 @@ package hr.unipu.journals.feature.manuscript.core
 import net.lingala.zip4j.ZipFile
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.File
 import java.nio.file.Files
 
 @Service
 class ZipService {
-    fun isEncrypted(file: MultipartFile): Boolean {
-        val temp = Files.createTempFile("upload", ".zip").toFile().apply { deleteOnExit() }
-        try {
-            file.inputStream.use { it.copyTo(temp.outputStream()) }
-            val zipFile = ZipFile(temp)
-            zipFile.fileHeaders.forEach { if(it.isEncrypted) return true }
-        } catch (_: Exception) { return true }
-        finally { temp.delete() }
+    fun isEncrypted(file: File): Boolean {
+        val zipFile = ZipFile(file)
+        zipFile.fileHeaders.forEach { if(it.isEncrypted) return true }
         return false
     }
 }
