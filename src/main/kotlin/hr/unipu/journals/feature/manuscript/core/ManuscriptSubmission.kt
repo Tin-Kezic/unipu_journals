478b59e7ad5347ee578df30925c1d9bf05812904
Tin Kežić <kezic.tin@gmail.com>
2026-01-13 15:57:34 +0100

    hotfix

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptSubmission.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptSubmission.kt
@@ -3,7 +3,7 @@ package hr.unipu.journals.feature.manuscript.core
 data class ManuscriptSubmission(
     val title: String,
     val category: String,
-    val publicationName: String,
+    val publicationTitle: String,
     val sectionName: String,
     val authors: List<AuthorDTO>,
     val correspondingAuthorEmail: String,
