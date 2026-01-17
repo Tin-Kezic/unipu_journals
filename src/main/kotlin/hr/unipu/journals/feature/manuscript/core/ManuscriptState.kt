d81a357a95f2dcf5d284a2d79810f96400ecd637
Tin Kežić <kezic.tin@gmail.com>
2026-01-17 01:05:02 +0100

    added AWAITING_ROUND_INITIALIZATION to ManuscriptState.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptState.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/core/ManuscriptState.kt
@@ -3,6 +3,7 @@ package hr.unipu.journals.feature.manuscript.core
 enum class ManuscriptState {
     AWAITING_EIC_REVIEW,
     AWAITING_EDITOR_REVIEW,
+    AWAITING_ROUND_INITIALIZATION,
     AWAITING_REVIEWER_REVIEW,
     MINOR,
     MAJOR,
