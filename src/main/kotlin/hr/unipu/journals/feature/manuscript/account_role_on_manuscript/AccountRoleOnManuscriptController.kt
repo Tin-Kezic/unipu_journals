b151bbc22bb4324046ab32a3270cded1f09ccabe
Tin Kežić <kezic.tin@gmail.com>
2026-01-18 00:26:13 +0100

    implemented AccountRoleOnManuscriptController.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptController.kt
@@ -1,7 +1,51 @@
 package hr.unipu.journals.feature.manuscript.account_role_on_manuscript
 
+import hr.unipu.journals.EmailService
+import hr.unipu.journals.feature.account.AccountRepository
+import hr.unipu.journals.feature.invite.InvitationTarget
+import hr.unipu.journals.feature.invite.InviteRepository
+import hr.unipu.journals.security.AuthorizationService
+import org.jsoup.Jsoup
+import org.jsoup.safety.Safelist
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AccountRoleOnManuscriptController {
+@RequestMapping("/api/account-role-on-manuscript")
+class AccountRoleOnManuscriptController(
+    private val accountRepository: AccountRepository,
+    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
+    private val inviteRepository: InviteRepository,
+    private val authorizationService: AuthorizationService,
+    private val emailService: EmailService
+) {
+    @PostMapping
+    fun assign(
+        @RequestParam email: String,
+        @RequestParam role: ManuscriptRole,
+        @RequestParam manuscriptId: Int
+    ): ResponseEntity<String> {
+        val cleanEmail = Jsoup.clean(email, Safelist.none())
+        when(role) {
+            ManuscriptRole.EIC -> require(authorizationService.isEicOnManuscript(manuscriptId))
+            ManuscriptRole.EDITOR -> require(authorizationService.isEditorOnManuscriptOrAffiliatedSuperior(manuscriptId))
+            ManuscriptRole.REVIEWER -> require(authorizationService.isReviewerOnManuscriptOrAffiliatedSuperior(manuscriptId))
+            ManuscriptRole.AUTHOR -> return ResponseEntity.badRequest().body("cannot manually assign author to manuscript")
+        }
+        accountRepository.byEmail(cleanEmail)?.let {
+            accountRoleOnManuscriptRepository.assign(
+                accountRole = role,
+                accountId = it.id,
+                manuscriptId = manuscriptId
+            )
+        } ?: inviteRepository.invite(
+            email = cleanEmail,
+            target = InvitationTarget.valueOf(role.name.replace("EIC", "EIC_ON_MANUSCRIPT")),
+            targetId = manuscriptId
+        )
+        return ResponseEntity.ok("successfully assigned role")
+    }
 }
