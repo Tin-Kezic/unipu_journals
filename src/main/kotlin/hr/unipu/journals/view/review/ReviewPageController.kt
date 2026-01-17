a3e11cd86fd1f5b52ead63a60c9ed136938d8105
Tin Kežić <kezic.tin@gmail.com>
2026-01-17 21:40:05 +0100

    deleted ReviewPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/review/ReviewPageController.kt
+++
@@ -1,9 +0,0 @@
-package hr.unipu.journals.view.review
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-
-@Controller
-class ReviewPageController() {
-    @GetMapping("/review/manuscript/{manuscriptReviewId}")
-    fun page() = "review/review-page"
-}
