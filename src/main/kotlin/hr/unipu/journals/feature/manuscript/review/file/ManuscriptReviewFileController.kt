18178170c154cc4c8a51d327377a3f12d20b6504
Tin Kežić <kezic.tin@gmail.com>
2026-02-23 22:46:23 +0100

    created ManuscriptReviewFileController.kt

---
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/review/file/ManuscriptReviewFileController.kt
@@ -0,0 +1,8 @@
+package hr.unipu.journals.feature.manuscript.review.file
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ManuscriptReviewFileController {
+
+}
