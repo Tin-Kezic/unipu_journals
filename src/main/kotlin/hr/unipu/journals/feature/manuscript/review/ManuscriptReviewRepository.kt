99605d8986ff08b7d2919f0b59dbe75987c35010
Tin Kežić <kezic.tin@gmail.com>
2026-02-23 22:53:00 +0100

    modified ManuscriptReviewRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/review/ManuscriptReviewRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/review/ManuscriptReviewRepository.kt
@@ -6,15 +6,33 @@ import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
 interface ManuscriptReviewRepository: Repository<ManuscriptReview, Int> {
-    @Query("SELECT * FROM manuscript_review WHERE manuscript_id = :manuscript_id AND round = :round AND reviewer_id = :reviewer_id")
-    fun by(
-        @Param("manuscript_id") manuscriptId: Int,
-        @Param("reviewer_id") reviewerId: Int,
-        @Param("round") round: Int
-    ): ManuscriptReview
+    @Query("""
+        SELECT manuscript_review.* FROM manuscript_review
+        JOIN manuscript_review_round ON manuscript_review.manuscript_review_round_id = manuscript_review_round.id
+        WHERE (manuscript_review_round.manuscript_id = :manuscript_id OR :manuscript_id IS NULL)
+        AND (manuscript_review_round.round = :round OR :round IS NULL)
+        AND (manuscript_review.reviewer_id = :reviewer_id OR :reviewer_id IS NULL)
+    """)
+    fun all(
+        @Param("manuscript_id") manuscriptId: Int? = null,
+        @Param("reviewer_id") reviewerId: Int? = null,
+        @Param("round") round: Int? = null
+    ): List<ManuscriptReview>
 
-    @Query("SELECT reviewer_id, round FROM manuscript_review WHERE manuscript_id = :manuscript_id")
-    fun reviewersAndRounds(@Param("manuscript_id") manuscriptId: Int): List<ReviewerAndRound>
+    @Query("SELECT * FROM manuscript_review WHERE id = :id")
+    fun byId(@Param("id") id: Int): ManuscriptReview?
+
+    @Query("""
+        SELECT EXISTS(
+            SELECT 1 FROM manuscript_review
+            WHERE manuscript_review_round_id = :manuscript_review_round_id
+            AND reviewer_id = :reviewer_id
+            AND overall_mark IS NOT NULL)
+    """)
+    fun isComplete(
+        @Param("manuscript_review_round_id") manuscriptReviewRoundId: Int,
+        @Param("reviewer_id") reviewerId: Int,
+    ): Boolean
 
     @Modifying
     @Query("INSERT INTO manuscript_review (reviewer_id, manuscript_review_round_id) VALUES (:reviewer_id, :manuscript_review_round_id)")
@@ -23,40 +41,39 @@ interface ManuscriptReviewRepository: Repository<ManuscriptReview, Int> {
         @Param("manuscript_review_round_id") manuscriptReviewRoundId: Int,
     ): Int
 
-    @Modifying
     @Query("""
         UPDATE manuscript_review SET
-            novelty = :#(#dto.novelty)::one_to_five,
-            significance = :#(#dto.significance)::one_to_five,
-            technical_quality = :#(#dto.technicalQuality)::one_to_five,
-            clarity = :#(#dto.clarity)::one_to_five,
-            methodology = :#(#dto.methodology)::one_to_five,
-            relevance_to_the_publication = :#(#dto.relevanceToThePublication)::one_to_five,
-            language_quality = :#(#dto.languageQuality)::one_to_five,
-            overall_mark = :#(#dto.overallMark)::one_to_five,
-            sufficient_background = :#(#dto.sufficientBackground)::review_question,
-            appropriate_research_design = :#(#dto.appropriateResearchDesign)::review_question,
-            adequately_described = :#(#dto.adequatelyDescribed)::review_question,
-            clearly_presented = :#(#dto.clearlyPresented)::review_question,
-            supported_conclusions = :#(#dto.supportedConclusions)::review_question,
-            conflict = :#(#dto.conflict),
-            plagiarism = :#(#dto.plagiarism),
-            llm = :#(#dto.llm),
-            self_citation = :#(#dto.selfCitation),
-            appropriate_references = :#(#dto.appropriateReferences),
-            ethical_concerns = :#(#dto.ethicalConcerns),
-            reviewer_comment = :#(#dto.reviewerComment),
-            reviewer_comment_file_url = :#(#dto.reviewerCommentFileUrl),
-            recommendation = :#(#dto.recommendation)::review_recommendation,,
+            novelty = :#{#dto.novelty?.name}::one_to_five,
+            significance = :#{#dto.significance?.name}::one_to_five,
+            technical_quality = :#{#dto.technicalQuality?.name}::one_to_five,
+            clarity = :#{#dto.clarity?.name}::one_to_five,
+            methodology = :#{#dto.methodology?.name}::one_to_five,
+            relevance_to_the_publication = :#{#dto.relevanceToThePublication?.name}::one_to_five,
+            language_quality = :#{#dto.languageQuality?.name}::one_to_five,
+            overall_mark = :#{#dto.overallMark?.name}::one_to_five,
+            sufficient_background = :#{#dto.sufficientBackground?.name}::review_question,
+            appropriate_research_design = :#{#dto.appropriateResearchDesign?.name}::review_question,
+            adequately_described = :#{#dto.adequatelyDescribed?.name}::review_question,
+            clearly_presented = :#{#dto.clearlyPresented?.name}::review_question,
+            supported_conclusions = :#{#dto.supportedConclusions?.name}::review_question,
+            conflict = :#{#dto.conflictOfInterest},
+            plagiarism = :#{#dto.plagiarism},
+            llm = :#{#dto.llmUsed},
+            self_citation = :#{#dto.selfCitation},
+            appropriate_references = :#{#dto.appropriateReferences},
+            ethical_concerns = :#{#dto.ethicalConcerns},
+            reviewer_comment = :#{#dto.reviewerComment},
+            recommendation = :#{#dto.recommendation?.name}::review_recommendation,
             review_date = CURRENT_TIMESTAMP
         WHERE manuscript_review_round_id = :manuscript_review_round_id
         AND reviewer_id = :reviewer_id
+        RETURNING *
     """)
     fun review(
         @Param("manuscript_review_round_id") manuscriptReviewRoundId: Int,
         @Param("reviewer_id") reviewerId: Int,
         @Param("dto") manuscriptReviewDTO: ManuscriptReviewDTO
-    ): Int
+    ): ManuscriptReview
 
     @Modifying
     @Query("UPDATE manuscript_review SET author_response = :response, author_response_date = CURRENT_TIMESTAMP")
