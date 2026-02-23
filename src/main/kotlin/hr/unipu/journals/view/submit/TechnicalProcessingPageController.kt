5606737fee4eee8ab3822fce7443fd45d77b68f3
Tin Kežić <kezic.tin@gmail.com>
2026-02-23 22:47:19 +0100

    implemented TechnicalProcessingPageController.kt

--- src/main/kotlin/hr/unipu/journals/view/submit/TechnicalProcessingPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/submit/TechnicalProcessingPageController.kt
@@ -1,4 +1,17 @@
 package hr.unipu.journals.view.submit
 
+import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_EIC_ON_MANUSCRIPT_OR_SUPERIOR
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@PreAuthorize(AUTHORIZATION_SERVICE_IS_EIC_ON_MANUSCRIPT_OR_SUPERIOR)
+@RequestMapping("/manuscripts/{manuscriptId}/technical-processing")
 class TechnicalProcessingPageController {
+    @GetMapping
+    fun page(): String {
+        return "/submit/technical-processing-page"
+    }
 }
