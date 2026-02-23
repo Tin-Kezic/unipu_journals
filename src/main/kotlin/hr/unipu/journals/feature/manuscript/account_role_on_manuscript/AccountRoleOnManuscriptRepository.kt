44515b52e51b55965afa8b106e988c56841e26a8
Tin Kežić <kezic.tin@gmail.com>
2026-02-23 22:45:03 +0100

    added editorOnManuscript query method to AccountRoleOnManuscriptRepository.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/account_role_on_manuscript/AccountRoleOnManuscriptRepository.kt
@@ -8,6 +8,9 @@ import org.springframework.data.repository.query.Param
 interface AccountRoleOnManuscriptRepository : Repository<AccountRoleOnManuscript, Int> {
     @Query("SELECT * FROM account_role_on_manuscript WHERE manuscript_id = :manuscript_id AND account_role = 'EIC'")
     fun eicOnManuscript(@Param("manuscript_id") manuscriptId: Int): AccountRoleOnManuscript
+    @Query("SELECT * FROM account_role_on_manuscript WHERE manuscript_id = :manuscript_id AND account_role = 'EDITOR'")
+    fun editorOnManuscript(@Param("manuscript_id") manuscriptId: Int): AccountRoleOnManuscript
+
     @Query("""
         SELECT DISTINCT * FROM account_role_on_manuscript
         WHERE (manuscript_id = :manuscript_id OR :manuscript_id IS NULL)
