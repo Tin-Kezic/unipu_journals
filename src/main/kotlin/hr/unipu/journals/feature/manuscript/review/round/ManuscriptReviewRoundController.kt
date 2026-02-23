01ae098e062d9310efea8d9b9a7b7c1de482c93e
Tin Kežić <kezic.tin@gmail.com>
2026-02-23 22:50:29 +0100

    implemented ManuscriptReviewRoundController.kt

--- src/main/kotlin/hr/unipu/journals/feature/manuscript/review/round/ManuscriptReviewRoundController.kt
+++ src/main/kotlin/hr/unipu/journals/feature/manuscript/review/round/ManuscriptReviewRoundController.kt
@@ -1,6 +1,7 @@
 package hr.unipu.journals.feature.manuscript.review.round
 
 import hr.unipu.journals.EmailService
+import hr.unipu.journals.feature.account.AccountRepository
 import hr.unipu.journals.feature.manuscript.account_role_on_manuscript.AccountRoleOnManuscriptRepository
 import hr.unipu.journals.feature.manuscript.account_role_on_manuscript.ManuscriptRole
 import hr.unipu.journals.feature.manuscript.core.ManuscriptRepository
@@ -9,6 +10,8 @@ import hr.unipu.journals.feature.manuscript.core.ManuscriptState
 import hr.unipu.journals.feature.manuscript.review.ManuscriptReviewRepository
 import hr.unipu.journals.feature.manuscript.review.Recommendation
 import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_EDITOR_ON_MANUSCRIPT_OR_SUPERIOR
+import hr.unipu.journals.security.AuthorizationService
+import hr.unipu.journals.util.AppProperties
 import org.jsoup.Jsoup
 import org.jsoup.safety.Safelist
 import org.springframework.http.HttpStatus
@@ -24,12 +27,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/manuscripts/{manuscriptId}/review-rounds")
 @PreAuthorize(AUTHORIZATION_SERVICE_IS_EDITOR_ON_MANUSCRIPT_OR_SUPERIOR)
 class ManuscriptReviewRoundController(
+    private val accountRepository: AccountRepository,
+    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
     private val manuscriptService: ManuscriptService,
     private val manuscriptRepository: ManuscriptRepository,
-    private val accountRoleOnManuscriptRepository: AccountRoleOnManuscriptRepository,
     private val manuscriptReviewRoundRepository: ManuscriptReviewRoundRepository,
     private val manuscriptReviewRepository: ManuscriptReviewRepository,
-    private val emailService: EmailService
+    private val authorizationService: AuthorizationService,
+    private val emailService: EmailService,
+    private val appProperties: AppProperties
 ) {
     @PostMapping("/start")
     fun startRound(@PathVariable manuscriptId: Int): ResponseEntity<String> {
@@ -40,9 +46,12 @@ class ManuscriptReviewRoundController(
         if(response.statusCode != HttpStatus.OK) return response
         val round = manuscriptReviewRoundRepository.startRound(manuscriptId)
             ?: return ResponseEntity.internalServerError().body("failed to find round")
-        accountRoleOnManuscriptRepository.all(manuscriptId = manuscriptId, role = ManuscriptRole.REVIEWER).forEach { reviewer ->
+        val eicId = accountRoleOnManuscriptRepository.eicOnManuscript(manuscriptId).accountId
+        val editorId = accountRoleOnManuscriptRepository.editorOnManuscript(manuscriptId).accountId
+        val reviewerIds = accountRoleOnManuscriptRepository.all(manuscriptId = manuscriptId, role = ManuscriptRole.REVIEWER).map { it.accountId }
+        (setOf(eicId, editorId) + reviewerIds).forEach { reviewerId ->
             val rowsAffected = manuscriptReviewRepository.insert(
-                reviewerId = reviewer.id,
+                reviewerId = reviewerId,
                 manuscriptReviewRoundId = round.id
             )
             if(rowsAffected == 0) return ResponseEntity.internalServerError().body("failed to assign reviewer")
@@ -53,28 +62,51 @@ class ManuscriptReviewRoundController(
     fun endRound(
         @PathVariable manuscriptId: Int,
         @RequestParam recommendation: Recommendation,
-        @RequestParam comment: String,
+        @RequestParam editorComment: String,
     ): ResponseEntity<String> {
-        val cleanedComment = Jsoup.clean(comment, Safelist.none())
+        val cleanedComment = Jsoup.clean(editorComment, Safelist.none())
         val rowsModified = manuscriptReviewRoundRepository.endRound(
             manuscriptId = manuscriptId,
             recommendation = recommendation,
             comment = cleanedComment
         )
         if(rowsModified == 0) return ResponseEntity.internalServerError().body("failed to end round")
-        if(recommendation in listOf(Recommendation.MINOR, Recommendation.MAJOR)) {
-            val manuscript = manuscriptRepository.byId(manuscriptId)
-                ?: return ResponseEntity.internalServerError().body("failed to find manuscript")
-            val response = manuscriptService.updateState(
-                manuscriptId = manuscriptId,
-                newState = ManuscriptState.valueOf(recommendation.name)
-            )
-            if(response.statusCode != HttpStatus.OK) return ResponseEntity.internalServerError().body("failed to end round")
-            emailService.sendHtml(
-                to = manuscript.correspondingAuthorEmail,
-                subject = "Manuscript set to ${recommendation.name.lowercase()}",
-                html = "The manuscript ${manuscript.title} has been set to ${recommendation.name.lowercase()}.<br>Editor's comment:<blockquote>$cleanedComment</blockquote>"
-            )
+        val manuscript = manuscriptRepository.byId(manuscriptId)
+            ?: return ResponseEntity.internalServerError().body("failed to find manuscript")
+        val eicId = accountRoleOnManuscriptRepository.eicOnManuscript(manuscriptId).accountId
+        val isEic = authorizationService.account?.id == eicId
+        val newState = when {
+            recommendation in setOf(Recommendation.MINOR, Recommendation.MAJOR) -> ManuscriptState.valueOf(recommendation.name)
+            isEic && recommendation == Recommendation.ACCEPT -> ManuscriptState.PUBLISHED
+            isEic && recommendation == Recommendation.REJECT -> ManuscriptState.REJECTED
+            else -> ManuscriptState.AWAITING_ROUND_INITIALIZATION
+        }
+        val response = manuscriptService.updateState(
+            manuscriptId = manuscriptId,
+            newState = newState
+        )
+        if(response.statusCode != HttpStatus.OK) return response
+        emailService.sendHtml(
+            to = manuscript.correspondingAuthorEmail,
+            subject = "Manuscript set to ${newState.name.lowercase()}",
+            html = """
+                The manuscript ${manuscript.title} has been set to ${newState.name.lowercase()}.<br>
+                Editor's comment:<blockquote>$cleanedComment</blockquote>
+                <a href="${appProperties.baseUrl}/manuscripts/${manuscript.id}/review-history">see review history</a>
+            """.trimIndent()
+        )
+        if(isEic.not() && recommendation in setOf(Recommendation.ACCEPT, Recommendation.REJECT)) {
+            accountRepository.byId(eicId)?.let {
+                emailService.sendHtml(
+                    to = it.email,
+                    subject = "Manuscript round ended",
+                    html = """
+                        round on manuscript ${manuscript.title} has ended.<br>
+                        Editor's recommendation: ${recommendation}<br>Editor's comment:<blockquote>$cleanedComment</blockquote>
+                        <a href="${appProperties.baseUrl}/manuscripts/${manuscript.id}/review-history">see review history</a>
+                    """.trimIndent()
+                )
+            }
         }
         return ResponseEntity.ok("successfully ended round")
     }
