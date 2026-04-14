e0559b20ecc19b4bffe70f66fc9e347030eb6f3d
Tin Kežić <kezic.tin@gmail.com>
2026-04-14 02:02:16 +0200

    implemented technical processing page

--- src/main/kotlin/hr/unipu/journals/view/submit/TechnicalProcessingPageController.kt
+++ src/main/kotlin/hr/unipu/journals/view/submit/TechnicalProcessingPageController.kt
@@ -1,17 +1,43 @@
 package hr.unipu.journals.view.submit
 
+import hr.unipu.journals.feature.manuscript.category.CategoryRepository
+import hr.unipu.journals.feature.manuscript.core.ManuscriptRepository
+import hr.unipu.journals.feature.manuscript.core.ManuscriptService
+import hr.unipu.journals.feature.manuscript.core.ManuscriptStateFilter
+import hr.unipu.journals.feature.publication.core.PublicationRepository
+import hr.unipu.journals.feature.section.core.SectionRepository
 import hr.unipu.journals.security.AUTHORIZATION_SERVICE_IS_EIC_ON_MANUSCRIPT_OR_SUPERIOR
+import hr.unipu.journals.view.ResourceNotFoundException
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 @PreAuthorize(AUTHORIZATION_SERVICE_IS_EIC_ON_MANUSCRIPT_OR_SUPERIOR)
-@RequestMapping("/manuscripts/{manuscriptId}/technical-processing")
-class TechnicalProcessingPageController {
-    @GetMapping
-    fun page(): String {
+class TechnicalProcessingPageController(
+    private val manuscriptRepository: ManuscriptRepository,
+    private val manuscriptService: ManuscriptService,
+    private val sectionRepository: SectionRepository,
+    private val publicationRepository: PublicationRepository,
+    private val categoryRepository: CategoryRepository
+) {
+    @GetMapping("/manuscripts/{manuscriptId}/technical-processing")
+    fun page(@PathVariable manuscriptId: Int, model: Model): String {
+        val manuscript = manuscriptRepository.byId(manuscriptId) ?: throw ResourceNotFoundException("failed to find manuscript")
+        val section = sectionRepository.byId(manuscript.sectionId) ?: throw ResourceNotFoundException("failed to find section")
+        val publication =  publicationRepository.by(id = section.publicationId) ?: throw ResourceNotFoundException("failed to find publication")
+        model["manuscript"] = manuscriptService.toManuscriptDto(manuscript)
+        model["publication"] = publication.title
+        model["publications"] = publicationRepository.all(manuscriptStateFilter = ManuscriptStateFilter.PUBLISHED).map { it.title }
+        model["section"] = section.title
+        model["sections"] = sectionRepository.all(
+            publicationId = section.publicationId,
+            manuscriptStateFilter =  ManuscriptStateFilter.PUBLISHED
+        ).map { it.title }
+        model["categories"] = categoryRepository.all()
         return "/submit/technical-processing-page"
     }
 }
